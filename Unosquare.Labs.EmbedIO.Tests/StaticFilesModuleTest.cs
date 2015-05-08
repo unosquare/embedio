319df58820e9001f7bbcd90d28c6482b2868ddf0
Geo Perez <geovanni.perez@gmail.com>
2015-05-08 09:42:47 -0500

    First NUnit test

---
+++ Unosquare.Labs.EmbedIO.Tests/StaticFilesModuleTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Unosquare.Labs.EmbedIO.Tests.Properties;
+
+namespace Unosquare.Labs.EmbedIO.Tests
+{
+    using System.IO;
+    using Unosquare.Labs.EmbedIO.Modules;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class StaticFilesModuleTest
+    {
+        private const string ServerAddress = "http://localhost:7777/";
+        protected string RootPath;
+
+        protected WebServer WebServer;
+        protected TestConsoleLog Logger = new TestConsoleLog();
+
+        [SetUp]
+        public void Init()
+        {
+            var assemblyPath = Path.GetDirectoryName(typeof (StaticFilesModuleTest).Assembly.Location);
+            RootPath = Path.Combine(assemblyPath, "html");
+
+            if (Directory.Exists(RootPath) == false)
+                Directory.CreateDirectory(RootPath);
+
+            if (File.Exists(Path.Combine(RootPath, "index.html")) == false)
+                File.WriteAllText(Path.Combine(RootPath, "index.html"), Resources.index);
+
+            WebServer = new WebServer(ServerAddress, Logger);
+            WebServer.RegisterModule(new StaticFilesModule(RootPath));
+            WebServer.RunAsync();
+        }
+
+        [Test]
+        public void GetIndex()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(ServerAddress);
+            var response = (HttpWebResponse)request.GetResponse();
+
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Unosquare.Labs.EmbedIO.Tests/TestConsoleLog.cs
@@ -0,0 +1,46 @@
+﻿namespace Unosquare.Labs.EmbedIO.Tests
+{
+    using System;
+    using System.Collections.Concurrent;
+
+    public class TestConsoleLog : Unosquare.Labs.EmbedIO.Log.ILog
+    {
+        public static ConcurrentBag<string> Data = new ConcurrentBag<string>();
+
+        public void Info(object message)
+        {
+            Data.Add(message.ToString());
+        }
+
+        public void Error(object message)
+        {
+            Data.Add(message.ToString());
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            Data.Add(message.ToString());
+            Data.Add(exception.ToString());
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            Data.Add(String.Format(format, args));
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            Data.Add(String.Format(format, args));
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            Data.Add(String.Format(format, args));
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            Data.Add(String.Format(format, args));
+        }
+    }
+}
