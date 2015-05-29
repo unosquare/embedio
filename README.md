e364e834f5db2eb17b41152c279b0f6c6bacfaf1
Geo Perez <geovanni.perez@gmail.com>
2015-05-29 15:56:03 -0500

    Add WebServerTest.cs

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
  [![Build Status](https://travis-ci.org/unosquare/embedio.svg?branch=master)](https://travis-ci.org/unosquare/embedio)
  [![Build status](https://ci.appveyor.com/api/projects/status/w59t7sct3a8ir96t?svg=true)](https://ci.appveyor.com/project/geoperez/embedio)
  [![NuGet version](https://badge.fury.io/nu/embedio.svg)](https://www.nuget.org/packages/Embedio)
+[![Coverage Status](https://coveralls.io/repos/unosquare/embedio/badge.svg?branch=master)](https://coveralls.io/r/unosquare/embedio?branch=master)
 
 ![EmbedIO](http://unosquare.github.io/embedio/images/embedio.png)
 
---
+++ Unosquare.Labs.EmbedIO.Tests/WebServerTest.cs
@@ -0,0 +1,70 @@
+﻿namespace Unosquare.Labs.EmbedIO.Tests
+{
+    using NUnit.Framework;
+    using System.Globalization;
+    using System.Linq;
+    using Unosquare.Labs.EmbedIO.Log;
+    using Unosquare.Labs.EmbedIO.Modules;
+    using Unosquare.Labs.EmbedIO.Tests.Properties;
+
+    [TestFixture]
+    public class WebServerTest
+    {
+        private const int DefaultPort = 8888;
+
+        [Test]
+        public void WebServerDefaultConstructor()
+        {
+            Assert.AreEqual((new WebServer()).Log.GetType(), typeof (NullLog), "Default log is NullLog");
+        }
+
+        [Test]
+        public void WebServerConstructorWithPortParam()
+        {
+            var instance = new WebServer(DefaultPort);
+
+            Assert.AreEqual(instance.UrlPrefixes.Count, 1, "It has one URL Prefix");
+            Assert.IsTrue(
+                instance.UrlPrefixes.First().Contains(DefaultPort.ToString(CultureInfo.InvariantCulture)),
+                "Construct with port number is correct");
+        }
+
+        [Test]
+        public void WebServerConstructorWithPortAndLogParam()
+        {
+            var instance = new WebServer(DefaultPort, new TestConsoleLog());
+
+            Assert.AreEqual(instance.UrlPrefixes.Count, 1, "It has one URL Prefix");
+            Assert.IsTrue(
+                instance.UrlPrefixes.First().Contains(DefaultPort.ToString(CultureInfo.InvariantCulture)),
+                "Port number is correct");
+            Assert.AreEqual(instance.Log.GetType(), typeof (TestConsoleLog), "Log type is correct");
+        }
+
+        [Test]
+        public void RegisterAndUnregisterModule()
+        {
+            var instance = new WebServer();
+            instance.RegisterModule(new LocalSessionModule());
+
+            Assert.AreEqual(instance.Modules.Count, 1, "It has one module");
+
+            instance.UnregisterModule(typeof (LocalSessionModule));
+
+            Assert.AreEqual(instance.Modules.Count, 0, "It has not modules");
+        }
+
+        [Test]
+        public void WebServerStaticMethodWithPortParam()
+        {
+            Assert.AreEqual(WebServer.Create(DefaultPort).Log.GetType(), typeof (NullLog), "Default log is NullLog");
+        }
+
+        [Test]
+        public void WebServerStaticMethodWithConsole()
+        {
+            Assert.AreEqual(WebServer.CreateWithConsole(Resources.ServerAddress).Log.GetType(),
+                typeof (SimpleConsoleLog), "Default log is NullLog");
+        }
+    }
+}
