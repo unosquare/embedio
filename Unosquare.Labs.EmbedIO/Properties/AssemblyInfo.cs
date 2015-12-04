9da5313c8579fee775e0b02c3ef8594f469c4251
Mario Di Vece <mario@unosquare.com>
2015-12-04 09:52:47 -0600

    adding signing key for strong naming and updating assembly properties (versioning)

--- Unosquare.Labs.EmbedIO/Properties/AssemblyInfo.cs
+++ Unosquare.Labs.EmbedIO/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Unosquare SA de CV")]
 [assembly: AssemblyProduct("Unosquare.Labs.EmbedIO")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Unosquare 2013-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.14.*")]
+[assembly: AssemblyFileVersion("1.0.14.0")]
