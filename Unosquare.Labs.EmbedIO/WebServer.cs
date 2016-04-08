9c4dcdb2971fd4fb5953f7dd7edb70eb09367f59
Elijah Kohrt <elijah.kohrt@blocherconsulting.com>
2016-04-09 07:43:47 +0900

    Fixed bug with listener waiting on cancellation token instead of having to wait for a new request to clean up properly.  It now cleans up right away.

--- Unosquare.Labs.EmbedIO/WebServer.cs
+++ Unosquare.Labs.EmbedIO/WebServer.cs
@@ -377,18 +377,23 @@
             this.Listener.Start();
 
             this.Log.Info("Started HTTP Listener");
-            this._listenerTask = Task.Run(async () =>
+            this._listenerTask = Task.Factory.StartNew(() =>
             {
                 while (this.Listener != null && this.Listener.IsListening)
                 {
-                    ct.ThrowIfCancellationRequested();
-
                     try
                     {
-                        var clientSocket = await Listener.GetContextAsync();
+                        var clientSocketTask = Listener.GetContextAsync();
+                        clientSocketTask.Wait(ct);
+                        var clientSocket = clientSocketTask.Result;
+                        
                         var clientTask =
                             Task.Factory.StartNew((context) => HandleClientRequest(context as HttpListenerContext, app),
                                 clientSocket, ct);
+                    } 
+                    catch (OperationCanceledException)
+                    {
+                        throw;
                     }
                     catch (Exception ex)
                     {
