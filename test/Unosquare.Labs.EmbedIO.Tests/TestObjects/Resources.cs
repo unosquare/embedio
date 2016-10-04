ce27a9b786539837da6034b2f662f9c47ad4ccaf
Edmundo Ramírez <edmundormz@gmail.com>
2016-10-04 11:11:53 -0500

    Change in WebServerUrl string

--- test/Unosquare.Labs.EmbedIO.Tests/TestObjects/Resources.cs
+++ test/Unosquare.Labs.EmbedIO.Tests/TestObjects/Resources.cs
@@ -5,11 +5,12 @@ namespace Unosquare.Labs.EmbedIO.Tests.TestObjects
     public static class Resources
     {
         private const string ServerAddress = "http://localhost:{0}/";
+        public static int Counter = 9699;        
         public static string GetServerAddress(){
             Interlocked.Increment(ref Counter);
             return string.Format(ServerAddress, Counter);
         }
-        public static int Counter = 9699;
+
         public const string WsServerAddress = "ws://localhost:7777/";
         
         public static readonly string SubIndex = @"<!DOCTYPE html>
