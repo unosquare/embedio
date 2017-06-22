5cea7211e4a700703d1f9d01872105d65f6e4b0f
Israel-94 <arnulfo_israel@hotmail.com>
2017-06-22 18:01:11 -0500

    Working on FluentTest

--- test/Unosquare.Labs.EmbedIO.Tests/FluentTest.cs
+++ test/Unosquare.Labs.EmbedIO.Tests/FluentTest.cs
@@ -4,6 +4,9 @@
     using System.Reflection;
     using Modules;
     using TestObjects;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
 
     [TestFixture]
     public class FluentTest
@@ -69,6 +72,44 @@
             Assert.AreEqual(webServer.Module<WebApiModule>().ControllersCount, 4, "It has four controllers");
 
             webServer.Dispose();
+        }
+
+        [Test]
+        public void FluentWithStaticFolderArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => {
+                Extensions.WithStaticFolderAt(null, TestHelper.SetupStaticFolder());
+            });                     
+        }
+
+        [Test]
+        public void FluentWithVirtualPaths()
+        {
+            Dictionary<string, string> paths = new Dictionary<string, string>();
+            paths.Add("/Server/web", TestHelper.SetupStaticFolder());
+            paths.Add("/Server/api", TestHelper.SetupStaticFolder());
+            paths.Add("/Server/database", TestHelper.SetupStaticFolder());
+
+            var webServer = WebServer.Create(WebServerUrl)
+                .WithVirtualPaths(paths);
+
+            Assert.IsNotNull(webServer);
+            Assert.AreEqual(webServer.Modules.Count, 1, "It has 1 modules loaded");
+            Assert.IsNotNull(webServer.Module<StaticFilesModule>(), "It has StaticFilesModule");
+            Assert.AreEqual(webServer.Module<StaticFilesModule>().VirtualPaths.Count, 3, "It has 3 Virtual Paths");
+        }
+
+        [Test]
+        public void FluentWithVirtualPathsArgumentException()
+        {
+            Dictionary<string, string> paths = new Dictionary<string, string>();
+            paths.Add("/Server/web", TestHelper.SetupStaticFolder());
+            paths.Add("/Server/api", TestHelper.SetupStaticFolder());
+            paths.Add("/Server/database", TestHelper.SetupStaticFolder());
+
+            Assert.Throws<ArgumentException>(() => {
+                Extensions.WithVirtualPaths(null, paths);
+            });
         }
     }
 }
