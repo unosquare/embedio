a83895407ed43a5f1e65d161d467206647864405
Geo Perez <geovanni.perez@gmail.com>
2017-07-10 15:37:56 -0500

    Add test helper

---
+++ test/Unosquare.Labs.EmbedIO.Tests/ExtensionTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Unosquare.Labs.EmbedIO.Tests
+{
+    [TestFixture]
+    public class ExtensionTest
+    {
+        [TestCase(CompressionMethod.Gzip)]
+        [TestCase(CompressionMethod.Deflate)]
+        [TestCase(CompressionMethod.None)]
+        public void CompressGzipTest(CompressionMethod method)
+        {
+            var buffer = System.Text.Encoding.UTF8.GetBytes("THIS IS DATA");
+
+            var compressBuffer = buffer.Compress(method, System.IO.Compression.CompressionMode.Compress);
+
+            Assert.IsNotNull(compressBuffer);
+
+            var uncompressBuffer = compressBuffer.Compress(method, System.IO.Compression.CompressionMode.Decompress);
+
+            Assert.IsNotNull(uncompressBuffer);
+            Assert.AreEqual(uncompressBuffer, buffer);
+        }
+    }
+}
