c554571cb89a9567a629aa240bfa78b817458bb6
Geo Perez <geovanni.perez@gmail.com>
2018-08-17 12:55:28 -0500

    Mock file

---
+++ test/Unosquare.Labs.EmbedIO.Tests/Mocks/TestWebServer.cs
@@ -0,0 +1,40 @@
+﻿namespace Unosquare.Labs.EmbedIO.Tests.Mocks
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Constants;
+
+    public class TestWebServer : IWebServer
+    {
+        public ISessionWebModule SessionModule { get; }
+        public RoutingStrategy RoutingStrategy { get; }
+        public ReadOnlyCollection<IWebModule> Modules { get; }
+
+        public T Module<T>() 
+            where T : class, IWebModule
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterModule(IWebModule module)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnregisterModule(Type moduleType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RunAsync(CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            // do nothing
+        }
+    }
+}
