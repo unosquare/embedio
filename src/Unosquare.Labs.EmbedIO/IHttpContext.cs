4875b90f4158b077de9f7441b7b4c0fe92d2d7f6
Geo Perez <geovanni.perez@gmail.com>
2018-08-17 14:28:52 -0500

    Add new interfaces

---
+++ src/Unosquare.Labs.EmbedIO/IHttpContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unosquare.Labs.EmbedIO
+{
+    public interface IHttpContext
+    {
+        IHttpRequest Request { get;  }
+
+        IHttpResponse Response { get; }
+    }
+}
