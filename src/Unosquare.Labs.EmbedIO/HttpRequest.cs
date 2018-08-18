d57718b56aa68cddf30811c40df177ed1f8a1a14
Geo Perez <geovanni.perez@gmail.com>
2018-08-17 21:12:29 -0500

    Update HttpRequest

--- src/Unosquare.Labs.EmbedIO/HttpRequest.cs
+++ src/Unosquare.Labs.EmbedIO/HttpRequest.cs
@@ -7,35 +7,73 @@ namespace Unosquare.Labs.EmbedIO
     using System.Collections.Specialized;
     using System.IO;
 
+    /// <summary>
+    /// Represents a wrapper for HttpListenerContext.Request
+    /// </summary>
+    /// <seealso cref="Unosquare.Labs.EmbedIO.IHttpRequest" />
     public class HttpRequest : IHttpRequest
     {
-        private readonly HttpListenerContext _context;
-        
+        private readonly HttpListenerRequest _request;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HttpRequest"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
         public HttpRequest(HttpListenerContext context)
         {
-            _context = context;
+            _request = context.Request;
         }
 
-        public NameValueCollection Headers { get; }
+        /// <inheritdoc />
+        public NameValueCollection Headers => _request.Headers;
 
-        public Version ProtocolVersion { get; }
-        public bool KeepAlive { get; }
+        /// <inheritdoc />
+        public Version ProtocolVersion => _request.ProtocolVersion;
+        
+        /// <inheritdoc />
+        public bool KeepAlive => _request.KeepAlive;
 
-        public CookieCollection Cookies { get; }
-        public string RawUrl { get; }
-        public NameValueCollection QueryString { get; }
-        public string HttpMethod { get; }
-        public Uri Url { get;  }
-        public bool HasEntityBody { get; }
-        public Stream InputStream { get; }
+        /// <inheritdoc />
+        public CookieCollection Cookies => _request.Cookies;
+        
+        /// <inheritdoc />
+        public string RawUrl => _request.RawUrl;
+        
+        /// <inheritdoc />
+        public NameValueCollection QueryString => _request.QueryString;
+        
+        /// <inheritdoc />
+        public string HttpMethod => _request.HttpMethod;
+        
+        /// <inheritdoc />
+        public Uri Url => _request.Url;
+        
+        /// <inheritdoc />
+        public bool HasEntityBody => _request.HasEntityBody;
+        
+        /// <inheritdoc />
+        public Stream InputStream => _request.InputStream;
 
-        public Encoding ContentEncoding { get; }
-        public IPEndPoint RemoteEndPoint { get; }
-        public bool IsLocal => _context.Request.IsLocal;
+        /// <inheritdoc />
+        public Encoding ContentEncoding => _request.ContentEncoding;
+        
+        /// <inheritdoc />
+        public IPEndPoint RemoteEndPoint => _request.RemoteEndPoint;
+        
+        /// <inheritdoc />
+        public bool IsLocal => _request.IsLocal;
+        
+        /// <inheritdoc />
         public string UserAgent { get; }
+        
+        /// <inheritdoc />
         public bool IsWebSocketRequest { get; set; }
+        
+        /// <inheritdoc />
         public IPEndPoint LocalEndPoint { get; }
-        public string ContentType { get; }
+        
+        /// <inheritdoc />
+        public string ContentType => _request.ContentType;
     }
 }
 #endif
