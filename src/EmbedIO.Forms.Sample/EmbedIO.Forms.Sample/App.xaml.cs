a5a6b27ba483bfdbf6caf0fc51e14ec0f7878e9e
Alliston Carlos Rosa Aleixo <alliston@outlook.com>
2018-10-28 09:56:57 -0300

    Created the Xamarin Forms sample

---
+++ src/EmbedIO.Forms.Sample/EmbedIO.Forms.Sample/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Unosquare.Labs.EmbedIO;
+using Unosquare.Labs.EmbedIO.Modules;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace EmbedIO.Forms.Sample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            Task.Factory.StartNew(async () =>
+            {
+                using (var server = new WebServer("http://localhost:8080"))
+                {
+                    server.RegisterModule(new LocalSessionModule());
+                    server.Module<StaticFilesModule>().UseRamCache = true;
+                    server.Module<StaticFilesModule>().DefaultExtension = ".html";
+                    server.Module<StaticFilesModule>().DefaultDocument = "index.html";
+                    await server.RunAsync();
+                }
+            });
+        }
+    }
+}
