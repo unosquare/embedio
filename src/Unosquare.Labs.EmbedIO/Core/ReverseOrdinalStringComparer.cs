622948bf14e2ccc4dc9f8ed8c5d5496e0959a4af
Riccardo De Agostini <riccardo@tenacom.it>
2019-04-22 20:12:22 +0200

    Refactor virtual path management of StaticFilesModule (fixes #272 et al.) (#275)
    * Avoid creating a temporary Dictionary.
    
    * Remove excess parenthesis.
    
    * Refactor virtual path management of StaticFilesModule (fixes #272 et al.)
    
    * Determinism:
      - always evaluate virtual path in reverse ordinal order;
      - normalize URL paths to simplify code and ensure consistent mapping.
    
    * Separation of concerns:
      - have methods that deal with mapping URL paths to local paths,
        and other methods that deal with the existence of local paths.
    
    * Caching:
      - keep track of _how_ and _why_ paths were mapped;
      - discard cached mapped paths whose generating data (virtual path,
        default extension, default documkent name) has changed.
    
    * Support for dynamic file systems:
      - allow the user to deactivate path caching in situations where the
        contents of served directories may change over time.
    
    * Compatibility:
      - keep existing constructors, methods, and properties of
        StaticFilesModule;
      - keep existing exception semantics (e.g. throw
        InvalidOperationException instead of ArgumentException for invalid
        paths passed to RegisterVirtualPath).
    
    * Behavior changes:
      - scenarios where files could be added to a served directory after
        being requested (resulting in error 404) are no longer supported:
        said files will continue to give error 404, unless path caching
        is disabled.
      - scenarios where files are continuously added and/or deleted,
        which previously resulted in spurious error 404s (#272), can now be
        supported by disabling path caching.
    
    * Modify test that would not throw on machines where drive E: actually exists.

---
+++ src/Unosquare.Labs.EmbedIO/Core/ReverseOrdinalStringComparer.cs
@@ -0,0 +1,19 @@
+﻿namespace Unosquare.Labs.EmbedIO.Core
+{
+    using System;
+    using System.Collections.Generic;
+
+    // Sorts strings in reverse order to obtain the evaluation order of virtual paths
+    internal sealed class ReverseOrdinalStringComparer : IComparer<string>
+    {
+        private static readonly IComparer<string> _directComparer = StringComparer.Ordinal;
+
+        private ReverseOrdinalStringComparer()
+        {
+        }
+
+        public static IComparer<string> Instance { get; } = new ReverseOrdinalStringComparer();
+
+        public int Compare(string x, string y) => _directComparer.Compare(y, x);
+    }
+}
---
+++ src/Unosquare.Labs.EmbedIO/Core/VirtualPath.cs
@@ -0,0 +1,42 @@
+﻿namespace Unosquare.Labs.EmbedIO.Core
+{
+    using System;
+    using System.IO;
+
+    internal sealed class VirtualPath
+    {
+        public VirtualPath(string baseUrlPath, string baseLocalPath)
+        {
+            BaseUrlPath = PathHelper.EnsureValidUrlPath(baseUrlPath, true);
+            try
+            {
+                BaseLocalPath = Path.GetFullPath(PathHelper.EnsureValidLocalPath(baseLocalPath));
+            }
+#pragma warning disable CA1031
+            catch (Exception e)
+            {
+                throw new InvalidOperationException($"Cannot determine the full local path for \"{baseLocalPath}\".", e);
+            }
+#pragma warning restore CA1031
+        }
+
+        public string BaseUrlPath { get; }
+
+        public string BaseLocalPath { get; }
+
+        internal bool CanMapUrlPath(string urlPath) => urlPath.StartsWith(BaseUrlPath, StringComparison.Ordinal);
+
+        internal bool TryMapUrlPathLoLocalPath(string urlPath, out string localPath)
+        {
+            if (!CanMapUrlPath(urlPath))
+            {
+                localPath = null;
+                return false;
+            }
+
+            var relativeUrlPath = urlPath.Substring(BaseUrlPath.Length);
+            localPath = Path.Combine(BaseLocalPath, relativeUrlPath.Replace('/', Path.DirectorySeparatorChar));
+            return true;
+        }
+    }
+}
