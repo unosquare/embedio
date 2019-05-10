ce23523f92137f93d0bcc4f9847182dbd0d2dc1c
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-10 12:05:37 +0200

    Add path parameter to WebHandler.

--- src/EmbedIO/WebHandler.cs
+++ src/EmbedIO/WebHandler.cs
@@ -4,10 +4,12 @@ using System.Threading.Tasks;
 namespace EmbedIO
 {
     /// <summary>
-    /// Represents a Web Handler.
+    /// Represents a callback used to handle a request.
     /// </summary>
-    /// <param name="context">The context.</param>
+    /// <param name="context">The context of the request.</param>
+    /// <param name="path">The requested path, either relative to the innermost containing module's <see cref="IWebModule.BaseUrlPath">BaseUrlPath</see> if any,
+    /// or equal to <paramref name="context"/><c>.Request.Url.AbsolutePath</c>.</param>
     /// <param name="ct">The cancellation token.</param>
-    /// <returns>A task representing the success of the web handler.</returns>
-    public delegate Task<bool> WebHandler(IHttpContext context, CancellationToken ct);
+    /// <returns>A task representing whether the web handler actually handled the request.</returns>
+    public delegate Task<bool> WebHandler(IHttpContext context, string path, CancellationToken ct);
 }
