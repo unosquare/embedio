a3e07fa23a1f43671e47116604326e584eda8271
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-10 07:54:22 +0200

    Simplify ActionModule; add RedirectModule.

--- src/EmbedIO/Modules/ActionModule.cs
+++ src/EmbedIO/Modules/ActionModule.cs
@@ -1,80 +1,39 @@
 ﻿using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using EmbedIO.Constants;
+using EmbedIO.Utilities;
 
 namespace EmbedIO.Modules
 {
     /// <summary>
-    /// Represents a module to fallback any request.
+    /// A module that passes requests to a callback.
     /// </summary>
     /// <seealso cref="WebModuleBase" />
     public class ActionModule : WebModuleBase
     {
-        private readonly WebHandler _webHandler;
-        private readonly HttpVerbs _httpVerb;
+        private readonly HttpVerbs _verb;
+
+        private readonly WebHandler _handler;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ActionModule" /> class.
         /// </summary>
         /// <param name="baseUrlPath">The base URL path.</param>
-        /// <param name="verb">The verb.</param>
-        /// <param name="action">The action.</param>
-        public ActionModule(string baseUrlPath, HttpVerbs verb, WebHandler action)
+        /// <param name="verb">The HTTP verb that will be served by this module.</param>
+        /// <param name="handler">The callback used to handle requests.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="handler"/> is <see langword="null"/>.</exception>
+        /// <seealso cref="WebModuleBase(string)"/>
+        public ActionModule(string baseUrlPath, HttpVerbs verb, WebHandler handler)
             : base(baseUrlPath)
         {
-            _webHandler = action;
-            _httpVerb = verb;
+            _verb = verb;
+            _handler = Validate.NotNull(nameof(handler), handler);
         }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ActionModule" /> class.
-        /// </summary>
-        /// <param name="baseUrlPath">The base URL path.</param>
-        /// <param name="redirectUrl">The redirect URL.</param>
-        /// <param name="verb">The verb.</param>
-        /// <exception cref="ArgumentNullException">redirectUrl.</exception>
-        public ActionModule(string baseUrlPath, string redirectUrl, HttpVerbs verb = HttpVerbs.Any)
-            : base(baseUrlPath)
-        {
-            if (string.IsNullOrWhiteSpace(redirectUrl))
-                throw new ArgumentNullException(nameof(redirectUrl));
-
-            RedirectUrl = redirectUrl;
-
-            _webHandler = (context, ct) => Task.FromResult(context.Redirect(redirectUrl));
-            _httpVerb = verb;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ActionModule" /> class.
-        /// </summary>
-        /// <param name="baseUrlPath">The base URL path.</param>
-        /// <param name="file">The file.</param>
-        /// <param name="contentType">Type of the content.</param>
-        /// <param name="verb">The verb.</param>
-        /// <exception cref="ArgumentNullException">file.</exception>
-        public ActionModule(string baseUrlPath, FileInfo file, string contentType = null,
-            HttpVerbs verb = HttpVerbs.Any)
-            : base(baseUrlPath)
-        {
-            if (file == null)
-                throw new ArgumentNullException(nameof(file));
-
-            _webHandler = (context, ct) => context.FileResponseAsync(file, contentType, true, ct);
-            _httpVerb = verb;
-        }
-
-        /// <summary>
-        /// Gets the redirect URL.
-        /// </summary>
-        public string RedirectUrl { get; }
 
         /// <inheritdoc />
-        public override Task<bool> HandleRequestAsync(IHttpContext context, string path, CancellationToken ct) =>
-            _httpVerb == HttpVerbs.Any || context.RequestVerb() == _httpVerb
-                ? _webHandler(context, ct)
-                : Task.FromResult(false);
+        public override async Task<bool> HandleRequestAsync(IHttpContext context, string path, CancellationToken ct) =>
+            (_verb == HttpVerbs.Any || context.RequestVerb() == _verb)
+            && await _handler(context, path, ct).ConfigureAwait(false);
     }
 }
