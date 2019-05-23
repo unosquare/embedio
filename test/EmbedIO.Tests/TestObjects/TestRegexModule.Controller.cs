902ed3f65a7e31166962d05facae1bd0e84b6081
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-23 11:12:18 +0200

    Update TestRegexModule in tests.

---
+++ test/EmbedIO.Tests/TestObjects/TestRegexModule.Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EmbedIO.Constants;
+using EmbedIO.Modules;
+
+namespace EmbedIO.Tests.TestObjects
+{
+    partial class TestRegexModule
+    {
+        public class Controller : WebApiController
+        {
+            public Controller(IHttpContext context, CancellationToken ct)
+                : base(context, ct)
+            {
+            }
+
+            [WebApiHandler(HttpVerbs.Any, "/data/{id!}")]
+            public Task<bool> Id(string id)
+                => HttpContext.Response.StringResponseAsync(id, "text/plain", Encoding.UTF8, false, CancellationToken);
+
+            [WebApiHandler(HttpVerbs.Any, "/data/{id!}/{time}")]
+            public Task<bool> Time(string id, string time)
+                => HttpContext.Response.StringResponseAsync(time, "text/plain", Encoding.UTF8, false, CancellationToken);
+
+            [WebApiHandler(HttpVerbs.Any, "/empty")]
+            public bool Empty()
+            {
+                HttpContext.Response.StandardResponseWithoutBody((int) HttpStatusCode.OK);
+                return true;
+            }
+        }
+    }
+}
--- test/EmbedIO.Tests/TestObjects/TestRegexModule.cs
+++ test/EmbedIO.Tests/TestObjects/TestRegexModule.cs
@@ -1,44 +1,13 @@
-﻿using System;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using EmbedIO.Modules;
 
 namespace EmbedIO.Tests.TestObjects
 {
-    public class TestRegexModule : WebModuleBase
+    public partial class TestRegexModule : WebApiModule
     {
         public TestRegexModule()
         : base("/")
         {
-        }
-
-        public override Task<bool> HandleRequestAsync(IHttpContext context, string path, CancellationToken ct)
-        {
-            // TODO: Riccardo, I'm not sure how this could be implemented
-            //AddHandler("/data/{id}", Constants.HttpVerbs.Any, (ctx, ct) =>
-            //{
-            //    var buffer = Encoding.UTF8.GetBytes(ctx.RequestRegexUrlParams("/data/{id}")["id"].ToString());
-            //    ctx.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
-            //    return Task.FromResult(true);
-            //});
-
-            //AddHandler("/data/{id}/{time}", Constants.HttpVerbs.Any, (ctx, ct) =>
-            //{
-            //    var buffer = Encoding.UTF8.GetBytes(ctx.RequestRegexUrlParams("/data/{id}/{time}")["time"].ToString());
-            //    ctx.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
-            //    return Task.FromResult(true);
-            //});
-
-            //AddHandler("/empty", Constants.HttpVerbs.Any, (ctx, ct) =>
-            //{
-            //    var buffer = Encoding.UTF8.GetBytes("data");
-            //    ctx.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
-            //    return Task.FromResult(true);
-            //});
-            throw new NotImplementedException();
+            RegisterController<Controller>();
         }
     }
 }
