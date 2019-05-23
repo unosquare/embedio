d759329e00374ea5a94d4de153cc126b979552e6
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-23 15:42:05 +0200

    Improve XML docs.

--- src/EmbedIO/WebHandler.cs
+++ src/EmbedIO/WebHandler.cs
@@ -4,12 +4,11 @@ using System.Threading.Tasks;
 namespace EmbedIO
 {
     /// <summary>
-    /// Represents a callback used to handle a request.
+    /// A callback used to handle a request.
     /// </summary>
-    /// <param name="context">The context of the request.</param>
-    /// <param name="path">The requested path, either relative to the innermost containing module's <see cref="IWebModule.BaseUrlPath">BaseUrlPath</see> if any,
-    /// or equal to <paramref name="context"/><c>.Request.Url.AbsolutePath</c>.</param>
-    /// <param name="ct">The cancellation token.</param>
-    /// <returns>A task representing whether the web handler actually handled the request.</returns>
+    /// <param name="context">A <see cref="IHttpContext"/> interface representing the context of the request.</param>
+    /// <param name="path">The requested path, relative to the innermost containing module's <see cref="IWebModule.BaseUrlPath">BaseUrlPath</see>.</param>
+    /// <param name="ct">A <see cref="CancellationToken"/> used to cancel the operation.</param>
+    /// <returns>A <see cref="Task"/> representing the ongoing operation, whose result will tell whether the request has been handled.</returns>
     public delegate Task<bool> WebHandler(IHttpContext context, string path, CancellationToken ct);
 }
