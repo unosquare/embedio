dd6a6097864110111f15095bd0cfa00fd3e0d02d
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-24 12:32:30 +0200

    [ADD] Base class for objects that can lock their configuration.

---
+++ src/EmbedIO/Utilities/ConfiguredObject.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EmbedIO.Modules;
+
+namespace EmbedIO.Utilities
+{
+    /// <summary>
+    /// Base class for objects whose configuration may be locked,
+    /// thus becoming read-only, at a certain moment in their lifetime.
+    /// </summary>
+    public abstract class ConfiguredObject
+    {
+        /// <summary>
+        /// Gets a value indicating whether s configuration has already been locked
+        /// and has therefore become read-only.
+        /// </summary>
+        /// <value>
+        /// <see langword="true"/> if the configuration is locked; otherwise, <see langword="false"/>.
+        /// </value>
+        /// <seealso cref="EnsureConfigurationNotLocked"/>
+        protected bool ConfigurationLocked { get; private set; }
+
+        /// <summary>
+        /// <para>Locks this instance's configuration, preventing further modifications.</para>
+        /// </summary>
+        /// <remarks>
+        /// <para>Configuration locking must be enforced by derived classes
+        /// by calling <see cref="EnsureConfigurationNotLocked"/> at the start
+        /// of methods and property setters that could change the object's
+        /// configuration.</para>
+        /// <para>This method may be called at any time, for example to prevent adding further controllers
+        /// to a <see cref="WebApiModule"/>-derived class.</para>
+        /// </remarks>
+        protected void LockConfiguration() => ConfigurationLocked = true;
+
+        /// <summary>
+        /// Checks whether a module's configuration has become read-only
+        /// and, if so, throws an <see cref="InvalidOperationException"/>.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">The configuration is locked.</exception>
+        /// <seealso cref="ConfigurationLocked"/>
+        protected void EnsureConfigurationNotLocked()
+        {
+            if (ConfigurationLocked)
+                throw new InvalidOperationException($"Configuration of this {GetType().Name} instance is locked.");
+        }
+    }
+}
--- src/EmbedIO/WebModuleBase.cs
+++ src/EmbedIO/WebModuleBase.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
-using EmbedIO.Modules;
 using EmbedIO.Utilities;
 
 namespace EmbedIO
@@ -14,13 +13,13 @@ namespace EmbedIO
     /// <item><description>validation and immutability of the <see cref="BaseUrlPath"/> property,
     /// which are of paramount importance for the correct functioning of a web server;</description></item>
     /// <item><description>support for configuration locking upon web server startup
-    /// (see the <see cref="ConfigurationLocked"/> property
-    /// and the <see cref="EnsureConfigurationNotLocked"/> method);</description></item>
+    /// (see the <see cref="ConfiguredObject.ConfigurationLocked"/> property
+    /// and the <see cref="ConfiguredObject.EnsureConfigurationNotLocked"/> method);</description></item>
     /// <item><description>a basic implementation of the <see cref="IWebModule.Start"/> method
     /// for modules that do not need to do anything upon web server startup.</description></item>
     /// </list>
     /// </summary>
-    public abstract class WebModuleBase : IWebModule
+    public abstract class WebModuleBase : ConfiguredObject, IWebModule
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="WebModuleBase"/> class.
@@ -39,6 +38,9 @@ namespace EmbedIO
         public string BaseUrlPath { get; }
 
         /// <inheritdoc />
+        /// <remarks>
+        /// <para>The module's configuration is locked before returning from this method.</para>
+        /// </remarks>
         public void Start(CancellationToken ct)
         {
             OnStart(ct);
@@ -54,43 +56,6 @@ namespace EmbedIO
         /// <param name="ct">A <see cref="CancellationToken"/> used to stop the web server.</param>
         protected virtual void OnStart(CancellationToken ct)
         {
-        }
-
-        /// <summary>
-        /// Gets a value indicating whether a module's configuration has already been locked
-        /// and has therefore become read-only.
-        /// </summary>
-        /// <value>
-        /// <see langword="true"/> if the configuration is locked; otherwise, <see langword="false"/>.
-        /// </value>
-        /// <seealso cref="EnsureConfigurationNotLocked"/>
-        protected bool ConfigurationLocked { get; private set; }
-
-        /// <summary>
-        /// <para>Locks the module's configuration, preventing further modifications.</para>
-        /// </summary>
-        /// <remarks>
-        /// <para>Configuration locking must be enforced by derived classes
-        /// by calling <see cref="EnsureConfigurationNotLocked"/> at the start
-        /// of methods and property setters that could change the module's
-        /// configuration.</para>
-        /// <para>This method may be called at any time, for example to prevent adding further controllers
-        /// to a <see cref="WebApiModule"/>-derived class; however, it is not
-        /// necessary to call it in all cases, because <see cref="WebModuleBase"/> calls it automatically
-        /// immediately after the <see cref="OnStart"/> method returns.</para>
-        /// </remarks>
-        protected void LockConfiguration() => ConfigurationLocked = true;
-
-        /// <summary>
-        /// Checks whether a module's configuration has become read-only
-        /// and, if so, throws an <see cref="InvalidOperationException"/>.
-        /// </summary>
-        /// <exception cref="InvalidOperationException">The configuration is locked.</exception>
-        /// <seealso cref="ConfigurationLocked"/>
-        protected void EnsureConfigurationNotLocked()
-        {
-            if (ConfigurationLocked)
-                throw new InvalidOperationException("Cannot configure a module once it has been started.");
         }
     }
 }
