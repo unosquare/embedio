efc2c0d9e54f9ea18bb30bb1a2f18efb0a69f8d8
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-24 11:02:28 +0200

    [ADD] Class RouteResolver to resolve a route / verb pair using a WebRouteHandler.

---
+++ src/EmbedIO/Routing/RouteResult.cs
@@ -0,0 +1,33 @@
+﻿namespace EmbedIO.Routing
+{
+    /// <summary>
+    /// Represents the outcome of route resolution by a <see cref="RouteResolver"/>.
+    /// </summary>
+    public enum RouteResult
+    {
+        /* DO NOT reorder members!
+         * RouteNotMatched < MethodNotMatched < OK
+         */
+
+        /// <summary>
+        /// The route didn't match, ot the handler returned <see langowrd="false"/>.
+        /// </summary>
+        RouteNotMatched,
+
+        /// <summary>
+        /// The route did match, but for a different HTTP method.
+        /// </summary>
+        MethodNotMatched,
+
+        /// <summary>
+        /// The route and HTTP verb were matched, but the route handler
+        /// returned <see langword="false"/>.
+        /// </summary>
+        RouteNotHandled,
+
+        /// <summary>
+        /// The route has been resolved.
+        /// </summary>
+        OK,
+    }
+}
