7e6e5806a7cf9fb345fa86da826783ac12676e07
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-27 08:06:52 +0200

    Disable unuseful warning about MD5 in sample program.

--- src/EmbedIO.Samples/Person.cs
+++ src/EmbedIO.Samples/Person.cs
@@ -16,6 +16,8 @@ namespace EmbedIO.Samples
         [LiteIndex]
         public string EmailAddress { get; set; }
 
+#pragma warning disable 0618 // "Use a better hasher." - Not our fault if gravatar.com uses MD5.
         public string PhotoUrl => $"http://www.gravatar.com/avatar/{EmailAddress.ComputeMD5().ToUpperHex()}.png?s=100";
+#pragma warning restore 0618
     }
 }
