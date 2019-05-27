2d81d2b88fc4fc4e0d171e820a6a26cfa6f06793
Riccardo De Agostini <riccardo@tenacom.it>
2019-05-27 17:13:48 +0200

    [ADD] RoutingModuleExtensions.WithHandlersFrom method.

--- src/EmbedIO/Routing/RoutingModuleExtensions.cs
+++ src/EmbedIO/Routing/RoutingModuleExtensions.cs
@@ -56,6 +56,22 @@ namespace EmbedIO.Routing
         }
 
         /// <summary>
+        /// <para>Adds handlers, associating them with HTTP method / route pairs by means
+        /// of <see cref="RouteHandlerAttribute">RouteHandler</see> attributes.</para>
+        /// <para>See <see cref="RouteVerbResolverCollection.AddFrom(object)"/> for further information.</para>
+        /// </summary>
+        /// <param name="this">The <see cref="RoutingModule"/> on which this method is called.</param>
+        /// <param name="target">Where to look for compatible handlers.</param>
+        /// <returns><paramref name="this"/> with handlers added.</returns>
+        /// <exception cref="NullReferenceException"><paramref name="this"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="target"/> is <see langword="null"/>.</exception>
+        public static RoutingModule WithHandlersFrom(this RoutingModule @this, object target)
+        {
+            @this.AddFrom(target);
+            return @this;
+        }
+
+        /// <summary>
         /// Associates all requests matching a route to a handler.
         /// </summary>
         /// <param name="this">The <see cref="RoutingModule"/> on which this method is called.</param>
