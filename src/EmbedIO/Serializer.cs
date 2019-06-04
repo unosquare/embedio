29e8b9376d7a5777bfb2cb5fd747cb745c3770f6
Riccardo De Agostini <riccardo@tenacom.it>
2019-06-04 17:31:47 +0200

    [ADD] Definition of a data serializer callback and standard serializer.

---
+++ src/EmbedIO/Serializer.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmbedIO
+{
+    /// <summary>
+    /// Provides standard serializer callbacks.
+    /// </summary>
+    /// <seealso cref="SerializerCallback"/>
+    public static class Serializer
+    {
+        /// <summary>
+        /// <para>The default serializer callback used by EmbedIO.</para>
+        /// <para>Equivalent to <see cref="Json"/>.</para>
+        /// </summary>
+        public static SerializerCallback Default = Json;
+
+        /// <summary>
+        /// Serializes data in JSON format to a HTTP response,
+        /// using the <see cref="Unosquare.Swan.Formatters.Json"/> utility class.
+        /// </summary>
+        /// <param name="context">The HTTP context of the request.</param>
+        /// <param name="data">The data to serialize.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the operation.</param>
+        /// <returns>A <see cref="Task"/> representing the ongoing operation.</returns>
+        public static async Task Json(IHttpContext context, object data, CancellationToken cancellationToken)
+        {
+            context.Response.ContentType = MimeTypes.JsonType;
+            using (var text = context.OpenResponseText(Encoding.UTF8))
+            {
+                await text.WriteAsync(Unosquare.Swan.Formatters.Json.Serialize(data, false)).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ src/EmbedIO/SerializerCallback.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EmbedIO
+{
+    /// <summary>
+    /// A callback used to serialize data to a HTTP response.
+    /// </summary>
+    /// <param name="context">The HTTP context of the request.</param>
+    /// <param name="data">The data to serialize.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> used to cancel the operation.</param>
+    /// <returns>A <see cref="Task"/> representing the ongoing operation.</returns>
+    public delegate Task SerializerCallback(IHttpContext context, object data, CancellationToken cancellationToken);
+}
