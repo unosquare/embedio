414caf1182943804504f266c653e8f08425d9a93
Riccardo De Agostini <riccardo@tenacom.it>
2019-06-04 15:31:11 +0200

    [ADD] More shortcut methods in HttpException.

--- src/EmbedIO/HttpException-Shortcuts.cs
+++ src/EmbedIO/HttpException-Shortcuts.cs
@@ -8,7 +8,13 @@ namespace EmbedIO
 
         public static HttpException Forbidden() => new HttpException(HttpStatusCode.Forbidden);
 
+        public static HttpException BadRequest() => new HttpException(HttpStatusCode.BadRequest);
+
         public static HttpException NotFound() => new HttpException(HttpStatusCode.NotFound);
+
+        public static HttpException MethodNotAllowed() => new HttpException(HttpStatusCode.MethodNotAllowed);
+
+        public static HttpException NotAcceptable() => new HttpException(HttpStatusCode.NotAcceptable);
 
         public static HttpRedirectException Redirect(string location, int statusCode = (int)HttpStatusCode.Found)
             => new HttpRedirectException(location, statusCode);
