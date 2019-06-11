ff4164e1906707044ad7915de118e638d91991ee
Riccardo De Agostini <riccardo@tenacom.it>
2019-06-11 21:14:02 +0200

    [ADD] Support types for the soon-to-come revamped file serving module.

---
+++ src/EmbedIO/Files/IFileProvider.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace EmbedIO.Files
+{
+    /// <summary>
+    /// Represents an object that can provide files and/or directories to be served by a <see cref="FileModule"/>.
+    /// </summary>
+    public interface IFileProvider
+    {
+        /// <summary>
+        /// Gets a value indicating whether the files and directories provided by this instance
+        /// will never change.
+        /// </summary>
+        bool IsImmutable { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether streams returned by the <see cref="OpenFile"/> method
+        /// will have their <see cref="Stream.CanSeek">CanSeek</see> property set to <see langword="true"/>.
+        /// </summary>
+        bool CanSeekFiles { get; }
+
+        /// <summary>
+        /// <para>Occurs when a file or directory provided by this instance is modified or removed.</para>
+        /// </summary>
+        event FileSystemEventHandler ResourceChanged;
+
+        /// <summary>
+        /// Maps a URL path to a provider-specific path.
+        /// </summary>
+        /// <param name="urlPath">The URL path.</param>
+        /// <returns>A provider-specific path identifying a file or directory,
+        /// or <see langword="null"/> if this instance cannot provide a resource associated
+        /// to <paramref name="urlPath"/>.</returns>
+        MappedResourceInfo MapUrlPath(string urlPath);
+
+        /// <summary>
+        /// Attempts to get information about a resource.
+        /// </summary>
+        /// <param name="path">The provider-specific path for the resource.</param>
+        /// <param name="info">When this method returns <see langword="true"/>,
+        /// either a <see cref="MappedFileInfo"/> object containing information about a file,
+        /// or a <see cref="MappedDirectoryInfo"/> object containing information about a directory;
+        /// when this method returns <see langword="false"/>, <see langword="null"/>.
+        /// This parameter is passed uninitialized.</param>
+        /// <returns><see langword="true"/> if <paramref name="path"/> identifies a file or directory;
+        /// otherwise, <see langword="false"/>.</returns>
+        bool TryGetInfo(string path, out MappedResourceInfo info);
+
+        /// <summary>
+        /// Opens a file for reading.
+        /// </summary>
+        /// <param name="path">The provider-specific path for the file.</param>
+        /// <returns>
+        /// <para>A readable <see cref="Stream"/> of the file's contents.</para>
+        /// <para>If the <see cref="CanSeekFiles"/> property is <see langword="true"/>,
+        /// the returned stream is expected to also be seekable.</para>
+        /// </returns>
+        Stream OpenFile(string path);
+
+        /// <summary>
+        /// Returns an enumeration of the entries of a directory.
+        /// </summary>
+        /// <param name="path">The provider-specific path for the directory.</param>
+        /// <returns>An enumeration of <see cref="MappedResourceInfo"/> objects identifying the entries
+        /// in the directory identified by <paramref name="path"/>.</returns>
+        IEnumerable<MappedResourceInfo> GetDirectoryEntries(string path);
+    }
+}
---
+++ src/EmbedIO/Files/MappedResourceInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EmbedIO.Files
+{
+    /// <summary>
+    /// Base class for resources returned by <see cref="IFileProvider"/>.
+    /// </summary>
+    public abstract class MappedResourceInfo
+    {
+        private protected MappedResourceInfo(string path, string name, DateTime lastWriteTimeUtc)
+        {
+            Path = path;
+            Name = name;
+            LastWriteTimeUtc = lastWriteTimeUtc;
+        }
+
+        /// <summary>
+        /// Gets a unique, provider-specific path for the resource.
+        /// </summary>
+        public string Path { get; }
+
+        /// <summary>
+        /// Gets the name of the resource, as it would appear in a directory listing.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Gets the UTC date and time of the last modification made to the resource.
+        /// </summary>
+        public DateTime LastWriteTimeUtc { get; }
+    }
+}
