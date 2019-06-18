59efe81d96dc4f468c5c908f479bb7f42f86602f
Riccardo De Agostini <riccardo@tenacom.it>
2019-06-18 19:32:06 +0200

    [ADD] Methods for validation of MIME types and media ranges.

--- src/EmbedIO/MimeType.cs
+++ src/EmbedIO/MimeType.cs
@@ -1,4 +1,7 @@
-﻿namespace EmbedIO
+﻿using System;
+using EmbedIO.Utilities;
+
+namespace EmbedIO
 {
     /// <summary>
     /// Provides constants for commonly-used MIME types and association between file extensions and MIME types.
@@ -30,5 +33,120 @@
         /// The MIME type for URL-encoded HTML forms.
         /// </summary>
         internal const string UrlEncodedForm = "application/x-www-form-urlencoded";
+
+        /// <summary>
+        /// Determines whether the specified string is a valid MIME type or media range.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="acceptMediaRange">If set to <see langword="true"/>, both media ranges
+        /// (e.g. <c>"text/*"</c>, <c>"*/*"</c>) and specific MIME types (e.g. <c>"text/html"</c>)
+        /// are considered valid; if set to <see langword="false"/>, only specific MIME types
+        /// are considered valid.</param>
+        /// <returns><see langword="true"/> if <paramref name="value"/> is valid,
+        /// according to the value of <paramref name="acceptMediaRange"/>;
+        /// otherwise, <see langword="false"/>.</returns>
+        public static bool IsMimeType(string value, bool acceptMediaRange)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            var slashPos = value.IndexOf('/');
+            if (slashPos < 0)
+                return false;
+
+            var isWildcardType = false;
+            var type = value.Substring(0, slashPos);
+            if (type == "*")
+            {
+                if (!acceptMediaRange)
+                    return false;
+
+                isWildcardType = true;
+            }
+            else if (!Validate.IsRfc2616Token(type))
+            {
+                return false;
+            }
+
+            var subtype = value.Substring(slashPos + 1);
+            return subtype == "*"
+                ? acceptMediaRange && isWildcardType
+                : Validate.IsRfc2616Token(subtype);
+        }
+
+        /// <summary>
+        /// Splits the specified MIME type or media range into type and subtype.
+        /// </summary>
+        /// <param name="mimeType">The MIME type or media range to split.</param>
+        /// <returns>A tuple of type and subtype.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="mimeType"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="mimeType"/> is not a valid
+        /// MIME type or media range.</exception>
+        public static (string type, string subtype) Split(string mimeType)
+            => UnsafeSplit(Validate.MimeType(nameof(mimeType), mimeType, true));
+
+        /// <summary>
+        /// Matches the specified MIME type to a media range.
+        /// </summary>
+        /// <param name="mimeType">The MIME type to match.</param>
+        /// <param name="mediaRange">The media range.</param>
+        /// <returns><see langword="true"/> if <paramref name="mediaRange"/> is either
+        /// the same as <paramref name="mimeType"/>, or has the same type and a subtype
+        /// of <c>"*"</c>, or is <c>"*/*"</c>.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <para><paramref name="mimeType"/> is <see langword="null"/>.</para>
+        /// <para>- or -</para>
+        /// <para><paramref name="mediaRange"/> is <see langword="null"/>.</para>
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// <para><paramref name="mimeType"/> is not a valid MIME type.</para>
+        /// <para>- or -</para>
+        /// <para><paramref name="mediaRange"/> is not a valid MIME media range.</para>
+        /// </exception>
+        public static bool IsInRange(string mimeType, string mediaRange)
+            => UnsafeIsInRange(
+                Validate.MimeType(nameof(mimeType), mimeType, false),
+                Validate.MimeType(nameof(mediaRange), mediaRange, true));
+
+        internal static (string type, string subtype) UnsafeSplit(string mimeType)
+        {
+            var slashPos = mimeType.IndexOf('/');
+            return (mimeType.Substring(0, slashPos), mimeType.Substring(slashPos + 1));
+        }
+
+        internal static bool UnsafeIsInRange(string mimeType, string mediaRange)
+        {
+            // A validated media range that starts with '*' can only be '*/*'
+            if (mediaRange[0] == '*')
+                return true;
+
+            var typeSlashPos = mimeType.IndexOf('/');
+            var rangeSlashPos = mediaRange.IndexOf('/');
+
+            if (typeSlashPos != rangeSlashPos)
+                return false;
+
+            for (var i = 0; i < typeSlashPos; i++)
+            {
+                if (mimeType[i] != mediaRange[i])
+                    return false;
+            }
+
+            // A validated token has at least 1 character,
+            // thus there must be at least 1 character after a slash.
+            if (mediaRange[rangeSlashPos + 1] == '*')
+                return true;
+
+            if (mimeType.Length != mediaRange.Length)
+                return false;
+
+            for (var i = typeSlashPos + 1; i < mimeType.Length; i++)
+            {
+                if (mimeType[i] != mediaRange[i])
+                    return false;
+            }
+
+            return true;
+        }
     }
 }
---
+++ src/EmbedIO/Utilities/Validate-MimeType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EmbedIO.Utilities
+{
+    /// <summary>
+    /// Provides validation methods for method arguments.
+    /// </summary>
+    public static partial class Validate
+    {
+        /// <summary>
+        /// <para>Ensures that a <see langword="string"/> argument is valid as MIME type or media range as defined by
+        /// <see href="https://tools.ietf.org/html/rfc7231#section-5.3.2">RFC7231, Section 5,3.2</see>.</para>
+        /// </summary>
+        /// <param name="argumentName">The name of the argument to validate.</param>
+        /// <param name="value">The value to validate.</param>
+        /// <param name="acceptMediaRange">If <see langword="true"/>, media ranges (i.e. strings of the form <c>*/*</c>
+        /// and <c>type/*</c>) are considered valid; otherwise, they are rejected as invalid.</param>
+        /// <returns><paramref name="value"/>, if it is a valid MIME type or media range.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">
+        /// <para><paramref name="value"/> is the empty string.</para>
+        /// <para>- or -</para>
+        /// <para><paramref name="value"/> is not a valid MIME type or media range.</para>
+        /// </exception>
+        public static string MimeType(string argumentName, string value, bool acceptMediaRange)
+        {
+            value = NotNullOrEmpty(argumentName, value);
+
+            if (!EmbedIO.MimeType.IsMimeType(value, acceptMediaRange))
+                throw new ArgumentException("MIME type is not valid.", argumentName);
+
+            return value;
+        }
+    }
+}
