007f2be9178952baea120ca037bd1d4fa2a21097
Riccardo De Agostini <riccardo@tenacom.it>
2019-07-04 07:03:28 +0200

    Add missing XML documentation.

--- src/EmbedIO/WebApi/FormDataAttribute.cs
+++ src/EmbedIO/WebApi/FormDataAttribute.cs
@@ -4,9 +4,17 @@ using System.Threading.Tasks;
 
 namespace EmbedIO.WebApi
 {
+    /// <summary>
+    /// Specified that a parameter of a controller method will receive a <see cref="Dictionary{TKey,TValue}">Dictionary</see>
+    /// of HTML form data, obtained by deserializing a request body with a content type of <c>application/x-www-form-urlencoded</c>.
+    /// This class cannot be inherited.
+    /// </summary>
+    /// <seealso cref="Attribute" />
+    /// <seealso cref="IRequestDataAttribute{TController,TData}" />
     [AttributeUsage(AttributeTargets.Parameter)]
     public sealed class FormDataAttribute : Attribute, IRequestDataAttribute<WebApiController, Dictionary<string, object>>
     {
+        /// <inheritdoc />
         public Task<Dictionary<string, object>> GetRequestDataAsync(WebApiController controller)
             => controller.HttpContext.GetRequestFormDataAsync(controller.CancellationToken);
     }
