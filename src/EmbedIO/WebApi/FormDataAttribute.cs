cc832b78a9984fac119ccf0094c787498b7d843f
Riccardo De Agostini <riccardo@tenacom.it>
2019-07-04 07:03:15 +0200

    [BRK] Make class FormDataAttribute sealed.

--- src/EmbedIO/WebApi/FormDataAttribute.cs
+++ src/EmbedIO/WebApi/FormDataAttribute.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 namespace EmbedIO.WebApi
 {
     [AttributeUsage(AttributeTargets.Parameter)]
-    public class FormDataAttribute : Attribute, IRequestDataAttribute<WebApiController, Dictionary<string, object>>
+    public sealed class FormDataAttribute : Attribute, IRequestDataAttribute<WebApiController, Dictionary<string, object>>
     {
         public Task<Dictionary<string, object>> GetRequestDataAsync(WebApiController controller)
             => controller.HttpContext.GetRequestFormDataAsync(controller.CancellationToken);
