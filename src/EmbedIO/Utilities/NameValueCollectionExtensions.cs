f34e2242988d04f8967ad6d461b22240210ec685
Riccardo De Agostini <riccardo@tenacom.it>
2019-07-04 08:58:21 +0200

    [FIX] Extension methods should throw NRE when @this is null.

--- src/EmbedIO/Utilities/NameValueCollectionExtensions.cs
+++ src/EmbedIO/Utilities/NameValueCollectionExtensions.cs
@@ -21,7 +21,7 @@ namespace EmbedIO.Utilities
         /// <returns><see langword="true"/> if at least one of the values for <paramref name="name"/>
         /// in the collection is equal to <paramref name="value"/>; otherwise, <see langword="false"/>.
         /// </returns>
-        /// <exception cref="ArgumentNullException"><paramref name="this"/> is <see langword="null"/>.</exception>
+        /// <exception cref="NullReferenceException"><paramref name="this"/> is <see langword="null"/>.</exception>
         /// <remarks>White space is trimmed from the start and end of each value before comparison.</remarks>
         /// <seealso cref="Contains(NameValueCollection,string,string,StringComparison)"/>
         public static bool Contains(this NameValueCollection @this, string name, string value)
@@ -41,13 +41,13 @@ namespace EmbedIO.Utilities
         /// <returns><see langword="true"/> if at least one of the values for <paramref name="name"/>
         /// in the collection is equal to <paramref name="value"/>; otherwise, <see langword="false"/>.
         /// </returns>
-        /// <exception cref="ArgumentNullException"><paramref name="this"/> is <see langword="null"/>.</exception>
+        /// <exception cref="NullReferenceException"><paramref name="this"/> is <see langword="null"/>.</exception>
         /// <remarks>White space is trimmed from the start and end of each value before comparison.</remarks>
         /// <seealso cref="Contains(NameValueCollection,string,string)"/>
         public static bool Contains(this NameValueCollection @this, string name, string value, StringComparison comparisonType)
         {
             value = value?.Trim();
-            return Validate.NotNull(nameof(@this), @this)[name]?.SplitByComma()
+            return @this[name]?.SplitByComma()
                .Any(val => string.Equals(val?.Trim(), value, comparisonType)) ?? false;
         }
     }
