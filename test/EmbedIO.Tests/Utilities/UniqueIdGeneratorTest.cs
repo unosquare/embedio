8281ec755517890c4abb0e04921ea8bd91e9b995
Riccardo De Agostini <riccardo@tenacom.it>
2019-07-07 21:23:07 +0200

    More unit tests for Utilities classes.

---
+++ test/EmbedIO.Tests/Utilities/UniqueIdGeneratorTest.cs
@@ -0,0 +1,25 @@
+﻿using EmbedIO.Utilities;
+using NUnit.Framework;
+
+namespace EmbedIO.Tests.Utilities
+{
+    public class UniqueIdGeneratorTest
+    {
+        [Test]
+        public void GetNext_ReturnsValidString()
+        {
+            var id = UniqueIdGenerator.GetNext();
+            Assert.IsNotNull(id);
+            Assert.IsNotEmpty(id);
+        }
+
+        [Test]
+        public void GetNext_ReturnsUniqueId()
+        {
+            var ids = new string[100];
+            for (var i = 0; i < ids.Length; i++)
+                ids[i] = UniqueIdGenerator.GetNext();
+            CollectionAssert.AllItemsAreUnique(ids);
+        }
+    }
+}
