6fd8df2ba4302ce2248ae8b5d638959a8628bbb0
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-09-01 17:26:57 -0500

    Fixed Unit test

--- test/EmbedIO.Tests/Issues/Issue355_ContentResponseLength.cs
+++ test/EmbedIO.Tests/Issues/Issue355_ContentResponseLength.cs
@@ -1,5 +1,4 @@
-﻿using EmbedIO.Testing;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,28 +8,30 @@ namespace EmbedIO.Tests.Issues
     public class Issue355_ContentResponseLength
     {
         [Test]
-        public Task ActionModule_Handle_ContentLengthProperly()
+        public async Task ActionModule_Handle_ContentLengthProperly()
         {
             var ok = Encoding.UTF8.GetBytes("content");
 
-            void Configure(IWebServer server) => server
-                .WithAction("/", HttpVerbs.Get, async context =>
+            using (var server = new WebServer(1234))
+            {
+                server.WithAction("/", HttpVerbs.Get, async context =>
                 {
                     context.Response.Headers[HttpHeaderNames.ContentLength] = ok.Length.ToString();
                     await context.Response.OutputStream.WriteAsync(ok, 0, ok.Length);
                 });
 
-            async Task Use(HttpClient client)
-            {
-                using (var response = await client.GetAsync($"/").ConfigureAwait(false))
-                {
-                    var responseArray = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+                server.RunAsync();
 
-                    Assert.AreEqual(ok[0], responseArray[0]);
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync("http://localhost:1234/").ConfigureAwait(false))
+                    {
+                        var responseArray = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+                        Assert.AreEqual(ok[0], responseArray[0]);
+                    }
                 }
             }
-
-            return TestWebServer.UseAsync(Configure, Use);
         }
     }
 }
