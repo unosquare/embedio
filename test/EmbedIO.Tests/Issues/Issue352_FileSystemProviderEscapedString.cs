5cf649cf27c50b300ec1900366f6be737d8b7ea7
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-09-01 17:15:06 -0500

    Add Issues tests_

---
+++ test/EmbedIO.Tests/Issues/Issue352_FileSystemProviderEscapedString.cs
@@ -0,0 +1,38 @@
+﻿using EmbedIO.Testing;
+using System.Net.Http;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace EmbedIO.Tests.Issues
+{
+    public class Issue352_FileSystemProviderEscapedString
+    {
+        [Test]
+        public Task FileSystemProvider_Handle_PathWithEscapedString()
+        {
+            const string ok = "Content";
+
+            var tempFolder = $"Folder {DateTime.Now.Ticks}";
+            var tempPathWithWhitespace = Path.Combine(Path.GetTempPath(), tempFolder);
+            Directory.CreateDirectory(tempPathWithWhitespace);
+            var tempFile = Path.Combine(tempPathWithWhitespace, "index.html");
+            File.WriteAllText(tempFile, ok);
+
+            void Configure(IWebServer server) => server
+                .WithStaticFolder("/", Path.GetTempPath(), true);
+
+            async Task Use(HttpClient client)
+            {
+                using (var response = await client.GetAsync($"/{tempFolder}").ConfigureAwait(false))
+                {
+                    var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    Assert.AreEqual(ok, responseString);
+                }
+            }
+
+            return TestWebServer.UseAsync(Configure, Use);
+        }
+    }
+}
---
+++ test/EmbedIO.Tests/Issues/Issue355_ContentResponseLength.cs
@@ -0,0 +1,36 @@
+﻿using EmbedIO.Testing;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbedIO.Tests.Issues
+{
+    public class Issue355_ContentResponseLength
+    {
+        [Test]
+        public Task ActionModule_Handle_ContentLengthProperly()
+        {
+            var ok = Encoding.UTF8.GetBytes("content");
+
+            void Configure(IWebServer server) => server
+                .WithAction("/", HttpVerbs.Get, async context =>
+                {
+                    context.Response.Headers[HttpHeaderNames.ContentLength] = ok.Length.ToString();
+                    await context.Response.OutputStream.WriteAsync(ok, 0, ok.Length);
+                });
+
+            async Task Use(HttpClient client)
+            {
+                using (var response = await client.GetAsync($"/").ConfigureAwait(false))
+                {
+                    var responseArray = await response.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+
+                    Assert.AreEqual(ok[0], responseArray[0]);
+                }
+            }
+
+            return TestWebServer.UseAsync(Configure, Use);
+        }
+    }
+}
