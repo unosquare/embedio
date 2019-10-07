a84eb8577324b7c4f86497a22eff9daadaa9772a
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-10-07 14:58:22 -0500

    [ADD] Unit test - Issue #389

---
+++ test/EmbedIO.Tests/Issues/Issue389_RequestContentLength.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EmbedIO.Tests.Issues
+{
+    public class Issue389_RequestContentLength
+    {
+        [Test]
+        public async Task ActionModuleReadsProperty_Handle_ContentLengthProperly()
+        {
+            const string DefaultUrl = "http://localhost:1234/";
+            const string Content = "content";
+
+            using var server = new WebServer(HttpListenerMode.EmbedIO, DefaultUrl);
+            server.WithAction("/", HttpVerbs.Post, async context => {
+                await context.SendDataAsync(context.Request.ContentLength64);
+            });
+
+            _ = server.RunAsync();
+
+            using var client = new HttpClient();
+            using var content = new StringContent(Content, Encoding.UTF8, "text/plain");
+            using var response = await client.PostAsync(DefaultUrl, content).ConfigureAwait(false);
+            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            Assert.AreEqual(Content.Length.ToString(), responseString);
+        }
+
+        [Test]
+        public async Task ActionModuleReadsKey_Handle_ContentLengthProperly()
+        {
+            const string DefaultUrl = "http://localhost:1234/";
+            const string Content = "content";
+
+            using var server = new WebServer(HttpListenerMode.EmbedIO, DefaultUrl);
+            server.WithAction("/", HttpVerbs.Post, async context => {
+                await context.SendDataAsync(context.Request.Headers[HttpHeaderNames.ContentLength]);
+            });
+
+            _ = server.RunAsync();
+
+            using var client = new HttpClient();
+            using var content = new StringContent(Content, Encoding.UTF8, "text/plain");
+            using var response = await client.PostAsync(DefaultUrl, content).ConfigureAwait(false);
+            var responseString = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            Assert.AreEqual(Content.Length.ToString(), responseString);
+        }
+    }
+}
