260a2ecb23ecd36a86e4259f9bd984199a811cd8
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-12-27 13:29:11 -0600

    Helping to my friend Kezito

---
+++ src/EmbedIO/Security/IPBanningExecutor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using Swan.Threading;
+
+namespace EmbedIO.Security
+{
+    public static class IPBanningExecutor
+    {
+        private static readonly ConcurrentDictionary<string, IPBanningConfiguration> Configurations = new ConcurrentDictionary<string, IPBanningConfiguration>();
+        private static readonly PeriodicTask? Purger;
+
+        static IPBanningExecutor()
+        {
+            Purger = new PeriodicTask(TimeSpan.FromMinutes(1), ct =>
+            {
+                foreach (var conf in Configurations.Keys)
+                {
+                    if (Configurations.TryGetValue(conf, out var instance))
+                        instance.Purge();
+                }
+
+                return Task.CompletedTask;
+            });
+        }
+
+        public static IPBanningConfiguration RetrieveInstance(string baseRoute) => Configurations.GetOrAdd(baseRoute, (x) => new IPBanningConfiguration());
+
+        public static bool TryGetInstance(string baseRoute, out IPBanningConfiguration configuration) => Configurations.TryGetValue(baseRoute, out configuration);
+    }
+}
--- src/EmbedIO/Security/IPBanningModule.cs
+++ src/EmbedIO/Security/IPBanningModule.cs
@@ -1,11 +1,7 @@
-﻿using EmbedIO.Utilities;
-using Swan;
-using Swan.Threading;
-using System;
-using System.Collections.Concurrent;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EmbedIO.Security
@@ -14,34 +10,16 @@ namespace EmbedIO.Security
     /// A module to ban clients by IP address, based on TCP requests-per-second or RegEx matches on log messages.
     /// </summary>
     /// <seealso cref="WebModuleBase" />
-    public class IPBanningModule : WebModuleBase
+    public class IPBanningModule : WebModuleBase, IDisposable
     {
         /// <summary>
         /// The default ban minutes.
         /// </summary>
         public const int DefaultBanMinutes = 30;
-        private static readonly ConcurrentDictionary<IPAddress, BanInfo> Blacklist = new ConcurrentDictionary<IPAddress, BanInfo>();
-        private static readonly ConcurrentBag<IPAddress> Whitelist = new ConcurrentBag<IPAddress>();
-        private static readonly ConcurrentBag<IIPBanningCriterion> BanningCriterions = new ConcurrentBag<IIPBanningCriterion>();
-        private static readonly PeriodicTask? Purger;
 
-        private readonly int _banMinutes;
+        private readonly IPBanningConfiguration _configuration;
 
         private bool _disposed;
-        
-        static IPBanningModule()
-        {
-            Purger = new PeriodicTask(TimeSpan.FromMinutes(1), ct =>
-                {
-                    PurgeBlackList();
-                    foreach (var criterion in BanningCriterions)
-                    {
-                        criterion.PurgeData();
-                    }
-
-                    return Task.CompletedTask;
-                });
-        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="IPBanningModule" /> class.
@@ -49,80 +27,109 @@ namespace EmbedIO.Security
         /// <param name="baseRoute">The base route.</param>
         /// <param name="whitelist">A collection of valid IPs that never will be banned.</param>
         /// <param name="banMinutes">Minutes that an IP will remain banned.</param>
-        public IPBanningModule(string baseRoute,
-            IEnumerable<string>? whitelist = null,
-            int banMinutes = DefaultBanMinutes)
+        public IPBanningModule(string baseRoute = "/",
+                               IEnumerable<string>? whitelist = null,
+                               int banMinutes = DefaultBanMinutes)
             : base(baseRoute)
         {
-            _banMinutes = banMinutes;
+            _configuration = IPBanningExecutor.RetrieveInstance(baseRoute);
+            _configuration.BanTime = banMinutes;
+
             AddToWhitelist(whitelist);
         }
 
         /// <inheritdoc />
         public override bool IsFinalHandler => false;
-        
+
         private IPAddress? ClientAddress { get; set; }
+
+        /// <summary>
+        /// Registers the criterion.
+        /// </summary>
+        /// <param name="criterion">The criterion.</param>
+        public void RegisterCriterion(IIPBanningCriterion criterion)
+        {
+            _configuration.RegisterCriterion(criterion);
+        }
+
+        /// <inheritdoc />
+        public void Dispose() =>
+            Dispose(true);
+
+        /// <inheritdoc />
+        protected override void OnStart(CancellationToken cancellationToken)
+        {
+            _configuration.Lock();
+
+            base.OnStart(cancellationToken);
+        }
 
         /// <summary>
         /// Gets the list of current banned IPs.
         /// </summary>
         /// <returns>A collection of <see cref="BanInfo"/> in the blacklist.</returns>
-        public static IEnumerable<BanInfo> GetBannedIPs() =>
-            Blacklist.Values.ToList();
+        public static IEnumerable<BanInfo> GetBannedIPs(string baseRoute = "/")
+            => IPBanningExecutor.TryGetInstance(baseRoute, out var instance) ? instance.BlackList : throw new ArgumentException(nameof(baseRoute));
 
         /// <summary>
         /// Tries to ban an IP explicitly.
         /// </summary>
         /// <param name="address">The IP address to ban.</param>
         /// <param name="banMinutes">Minutes that the IP will remain banned.</param>
+        /// <param name="baseRoute">The base route.</param>
         /// <param name="isExplicit"><c>true</c> if the IP was explicitly banned.</param>
         /// <returns>
-        ///     <c>true</c> if the IP was added to the blacklist; otherwise, <c>false</c>.
+        ///   <c>true</c> if the IP was added to the blacklist; otherwise, <c>false</c>.
         /// </returns>
-        public static bool TryBanIP(IPAddress address, int banMinutes, bool isExplicit = true) =>
-            TryBanIP(address, DateTime.Now.AddMinutes(banMinutes), isExplicit);
+        public static bool TryBanIP(IPAddress address, int banMinutes, string baseRoute = "/", bool isExplicit = true) =>
+            TryBanIP(address, DateTime.Now.AddMinutes(banMinutes), baseRoute, isExplicit);
 
         /// <summary>
         /// Tries to ban an IP explicitly.
         /// </summary>
         /// <param name="address">The IP address to ban.</param>
-        /// <param name="banDuration">A <see cref="TimeSpan"/> specifying the duration that the IP will remain banned.</param>
+        /// <param name="banDuration">A <see cref="TimeSpan" /> specifying the duration that the IP will remain banned.</param>
+        /// <param name="baseRoute">The base route.</param>
         /// <param name="isExplicit"><c>true</c> if the IP was explicitly banned.</param>
         /// <returns>
-        ///     <c>true</c> if the IP was added to the blacklist; otherwise, <c>false</c>.
+        ///   <c>true</c> if the IP was added to the blacklist; otherwise, <c>false</c>.
         /// </returns>
-        public static bool TryBanIP(IPAddress address, TimeSpan banDuration, bool isExplicit = true) =>
-            TryBanIP(address, DateTime.Now.Add(banDuration), isExplicit);
+        public static bool TryBanIP(IPAddress address, TimeSpan banDuration, string baseRoute = "/", bool isExplicit = true) =>
+            TryBanIP(address, DateTime.Now.Add(banDuration), baseRoute, isExplicit);
 
         /// <summary>
         /// Tries to ban an IP explicitly.
         /// </summary>
         /// <param name="address">The IP address to ban.</param>
-        /// <param name="banUntil">A <see cref="DateTime"/> specifying the expiration time of the ban.</param>
+        /// <param name="banUntil">A <see cref="DateTime" /> specifying the expiration time of the ban.</param>
+        /// <param name="baseRoute">The base route.</param>
         /// <param name="isExplicit"><c>true</c> if the IP was explicitly banned.</param>
         /// <returns>
-        ///     <c>true</c> if the IP was added to the blacklist; otherwise, <c>false</c>.
+        ///   <c>true</c> if the IP was added to the blacklist; otherwise, <c>false</c>.
         /// </returns>
-        public static bool TryBanIP(IPAddress address, DateTime banUntil, bool isExplicit = true)
+        /// <exception cref="ArgumentException">baseRoute</exception>
+        public static bool TryBanIP(IPAddress address, DateTime banUntil, string baseRoute = "/", bool isExplicit = true)
         {
+            if (!IPBanningExecutor.TryGetInstance(baseRoute, out var instance))
+                throw new ArgumentException(nameof(baseRoute));
+
             try
             {
-                Blacklist.AddOrUpdate(address,
+                instance.AddOrUpdateBlackList(address,
                     k =>
-                        new BanInfo()
-                        {
+                        new BanInfo() {
                             IPAddress = k,
                             ExpiresAt = banUntil.Ticks,
                             IsExplicit = isExplicit,
                         },
                     (k, v) =>
-                        new BanInfo()
-                        {
+                        new BanInfo() {
                             IPAddress = k,
                             ExpiresAt = banUntil.Ticks,
                             IsExplicit = isExplicit,
                         }
                 );
+
                 return true;
             }
             catch
@@ -135,51 +142,24 @@ namespace EmbedIO.Security
         /// Tries to unban an IP explicitly.
         /// </summary>
         /// <param name="address">The IP address.</param>
+        /// <param name="baseRoute">The base route.</param>
         /// <returns>
-        ///     <c>true</c> if the IP was removed from the blacklist; otherwise, <c>false</c>.
+        ///   <c>true</c> if the IP was removed from the blacklist; otherwise, <c>false</c>.
         /// </returns>
-        public static bool TryUnbanIP(IPAddress address) =>
-            Blacklist.TryRemove(address, out _);
-        
-        /// <inheritdoc />
-        public void Dispose() =>
-            Dispose(true);
+        /// <exception cref="ArgumentException">baseRoute</exception>
+        public static bool TryUnbanIP(IPAddress address, string baseRoute = "/")
+            => IPBanningExecutor.TryGetInstance(baseRoute, out var instance) ? instance.TryRemoveBlackList(address) : throw new ArgumentException(nameof(baseRoute));
 
         internal void AddToWhitelist(IEnumerable<string> whitelist) =>
-            AddToWhitelistAsync(whitelist).GetAwaiter().GetResult();
+            _configuration.AddToWhitelistAsync(whitelist).GetAwaiter().GetResult();
 
-        internal async Task AddToWhitelistAsync(IEnumerable<string> whitelist)
-        {
-            if (whitelist?.Any() != true)
-                return;
-
-            foreach (var address in whitelist)
-            {
-                var addressees = await IPParser.ParseAsync(address).ConfigureAwait(false);
-                foreach (var ipAddress in addressees.Where(x => !Whitelist.Contains(x)))
-                {
-                    Whitelist.Add(ipAddress);
-                }
-            }
-        }
-        
         /// <inheritdoc />
         protected override async Task OnRequestAsync(IHttpContext context)
         {
             ClientAddress = context.Request.RemoteEndPoint.Address;
-
-            if (Whitelist.Contains(ClientAddress))
-                return;
-
-            foreach (var criterion in BanningCriterions)
-            {
-                await criterion.UpdateData(ClientAddress);
-            }
-
-            if (Blacklist.ContainsKey(ClientAddress))
-                throw HttpException.Forbidden();
+            await _configuration.CheckClient(ClientAddress);
         }
-        
+
         /// <summary>
         /// Releases unmanaged and - optionally - managed resources.
         /// </summary>
@@ -187,22 +167,9 @@ namespace EmbedIO.Security
         protected virtual void Dispose(bool disposing)
         {
             if (_disposed) return;
-            if (disposing)
-            {
-                
-            }
+            if (disposing) { }
 
             _disposed = true;
-        }
-
-        private static void PurgeBlackList()
-        {
-            foreach (var k in Blacklist.Keys)
-            {
-                if (Blacklist.TryGetValue(k, out var info) &&
-                    DateTime.Now.Ticks > info.ExpiresAt)
-                    Blacklist.TryRemove(k, out _);
-            }
         }
     }
 }
