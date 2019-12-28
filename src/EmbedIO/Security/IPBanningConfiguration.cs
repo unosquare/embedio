16604b98ad02eef4e9fdca651aec7879b67fcd44
Geo Perez <1775792+geoperez@users.noreply.github.com>
2019-12-28 13:04:26 -0600

    WIP

--- src/EmbedIO/Security/IPBanningConfiguration.cs
+++ src/EmbedIO/Security/IPBanningConfiguration.cs
@@ -12,9 +12,9 @@ namespace EmbedIO.Security
     public class IPBanningConfiguration : ConfiguredObject
     {
         private readonly List<IIPBanningCriterion> _criterions = new List<IIPBanningCriterion>();
-        private readonly ConcurrentDictionary<IPAddress, BanInfo> BlacklistDictionary = new ConcurrentDictionary<IPAddress, BanInfo>();
-        private readonly ConcurrentBag<IPAddress> WhiteListBag = new ConcurrentBag<IPAddress>();
-        private readonly int _banTime;
+        private readonly ConcurrentDictionary<IPAddress, BanInfo> _blacklistDictionary = new ConcurrentDictionary<IPAddress, BanInfo>();
+        private readonly ConcurrentBag<IPAddress> _whiteListBag = new ConcurrentBag<IPAddress>();
+        private int _banTime;
 
         public int BanTime
         {
@@ -25,6 +25,8 @@ namespace EmbedIO.Security
                 _banTime = value;
             }
         }
+        
+        public List<BanInfo> BlackList => _blacklistDictionary.Values.ToList();
 
         public void RegisterCriterion(IIPBanningCriterion criterion)
         {
@@ -32,7 +34,6 @@ namespace EmbedIO.Security
             _criterions.Add(criterion);
         }
 
-        public List<BanInfo> BlackList => BlacklistDictionary.Values.ToList();
 
         public void Purge()
         {
@@ -45,18 +46,18 @@ namespace EmbedIO.Security
 
         public void PurgeBlackList()
         {
-            foreach (var k in BlacklistDictionary.Keys)
+            foreach (var k in _blacklistDictionary.Keys)
             {
-                if (BlacklistDictionary.TryGetValue(k, out var info) &&
+                if (_blacklistDictionary.TryGetValue(k, out var info) &&
                     DateTime.Now.Ticks > info.ExpiresAt)
-                    BlacklistDictionary.TryRemove(k, out _);
+                    _blacklistDictionary.TryRemove(k, out _);
             }
         }
 
         public void Lock() => LockConfiguration();
 
         public void AddOrUpdateBlackList(IPAddress address, Func<IPAddress, BanInfo> addValueFactory, Func<IPAddress, BanInfo, BanInfo> updateValueFactory)
-            => BlacklistDictionary.AddOrUpdate(address, addValueFactory, updateValueFactory);
+            => _blacklistDictionary.AddOrUpdate(address, addValueFactory, updateValueFactory);
 
         internal async Task AddToWhitelistAsync(IEnumerable<string> whitelist)
         {
@@ -67,16 +68,16 @@ namespace EmbedIO.Security
             {
                 var addressees = await IPParser.ParseAsync(address).ConfigureAwait(false);
 
-                foreach (var ipAddress in addressees.Where(x => !WhiteListBag.Contains(x)))
+                foreach (var ipAddress in addressees.Where(x => !_whiteListBag.Contains(x)))
                 {
-                    WhiteListBag.Add(ipAddress);
+                    _whiteListBag.Add(ipAddress);
                 }
             }
         }
 
         public async Task CheckClient(IPAddress clientAddress)
         {
-            if (WhiteListBag.Contains(clientAddress))
+            if (_whiteListBag.Contains(clientAddress))
                 return;
 
             foreach (var criterion in _criterions)
@@ -84,10 +85,10 @@ namespace EmbedIO.Security
                 await criterion.UpdateData(clientAddress);
             }
 
-            if (BlacklistDictionary.ContainsKey(clientAddress))
+            if (_blacklistDictionary.ContainsKey(clientAddress))
                 throw HttpException.Forbidden();
         }
 
-        public bool TryRemoveBlackList(IPAddress address) => BlacklistDictionary.TryRemove(address, out _);
+        public bool TryRemoveBlackList(IPAddress address) => _blacklistDictionary.TryRemove(address, out _);
     }
 }
--- src/EmbedIO/WebModuleContainerExtensions-Security.cs
+++ src/EmbedIO/WebModuleContainerExtensions-Security.cs
@@ -9,80 +9,36 @@ namespace EmbedIO
         /// <summary>
         /// Creates an instance of <see cref="IPBanningModule" /> and adds it to a module container.
         /// </summary>
-        /// <typeparam name="TContainer">The type of the container.</typeparam>
+        /// <typeparam name="TContainer">The type of the module container.</typeparam>
         /// <param name="this">The <typeparamref name="TContainer" /> on which this method is called.</param>
-        /// <param name="configure">A callback used to configure the module.</param>
+        /// <param name="whiteList">A collection of valid IPs that never will be banned.</param>
         /// <param name="banMinutes">Minutes that an IP will remain banned.</param>
-        /// <param name="maxRequestPerSecond">The maximum requests per second.</param>
-        /// <param name="maxMatchCount">The maximum number of regex matches before banning an IP.</param>
-        /// <param name="secondsMatchingPeriod">The matching period.</param>
         /// <returns>
         ///   <paramref name="this" /> with a <see cref="IPBanningModule" /> added.
         /// </returns>
         public static TContainer WithIPBanning<TContainer>(this TContainer @this,
-            Action<IPBanningModule>? configure = null,
-            int banMinutes = IPBanningModule.DefaultBanMinutes,
-            int maxRequestPerSecond = IPBanningModule.DefaultMaxRequestsPerSecond,
-            int maxMatchCount = IPBanningModule.DefaultMaxMatchCount,
-            int secondsMatchingPeriod = IPBanningModule.DefaultSecondsMatchingPeriod)
+            IEnumerable<string>? whiteList = null,
+            int banMinutes = IPBanningModule.DefaultBanMinutes)
             where TContainer : class, IWebModuleContainer =>
-            SetModule(@this, null, null, banMinutes, maxRequestPerSecond, maxMatchCount, secondsMatchingPeriod, configure);
+            WithIPBanning(@this, null, whiteList, banMinutes);
 
         /// <summary>
         /// Creates an instance of <see cref="IPBanningModule" /> and adds it to a module container.
         /// </summary>
         /// <typeparam name="TContainer">The type of the module container.</typeparam>
         /// <param name="this">The <typeparamref name="TContainer" /> on which this method is called.</param>
-        /// <param name="failRegex">A collection of regex to match log messages against.</param>
+        /// <param name="configure">The configure.</param>
+        /// <param name="whiteList">A collection of valid IPs that never will be banned.</param>
         /// <param name="banMinutes">Minutes that an IP will remain banned.</param>
-        /// <param name="maxRequestPerSecond">The maximum requests per second.</param>
-        /// <param name="maxMatchCount">The maximum number of regex matches before banning an IP.</param>
-        /// <param name="secondsMatchingPeriod">The matching period.</param>
+        /// <param name="">The .</param>
         /// <returns>
         ///   <paramref name="this" /> with a <see cref="IPBanningModule" /> added.
         /// </returns>
         public static TContainer WithIPBanning<TContainer>(this TContainer @this,
-            IEnumerable<string> failRegex,
-            int banMinutes = IPBanningModule.DefaultBanMinutes,
-            int maxRequestPerSecond = IPBanningModule.DefaultMaxRequestsPerSecond,
-            int maxMatchCount = IPBanningModule.DefaultMaxMatchCount,
-            int secondsMatchingPeriod = IPBanningModule.DefaultSecondsMatchingPeriod)
+            Action<IPBanningModule>? configure,
+            IEnumerable<string>? whiteList = null,
+            int banMinutes = IPBanningModule.DefaultBanMinutes)
             where TContainer : class, IWebModuleContainer =>
-            WithIPBanning(@this, failRegex, null, banMinutes, maxRequestPerSecond, maxMatchCount, secondsMatchingPeriod);
-
-        /// <summary>
-        /// Creates an instance of <see cref="IPBanningModule" /> and adds it to a module container.
-        /// </summary>
-        /// <typeparam name="TContainer">The type of the module container.</typeparam>
-        /// <param name="this">The <typeparamref name="TContainer" /> on which this method is called.</param>
-        /// <param name="failRegex">A collection of regex to match log messages against.</param>
-        /// <param name="whitelist">A collection of valid IPs that never will be banned.</param>
-        /// <param name="banMinutes">Minutes that an IP will remain banned.</param>
-        /// <param name="maxRequestPerSecond">The maximum requests per second.</param>
-        /// <param name="maxMatchCount">The maximum number of regex matches before banning an IP.</param>
-        /// <param name="secondsMatchingPeriod">The matching period.</param>
-        /// <returns>
-        ///   <paramref name="this" /> with a <see cref="IPBanningModule" /> added.
-        /// </returns>
-        public static TContainer WithIPBanning<TContainer>(this TContainer @this,
-            IEnumerable<string>? failRegex,
-            IEnumerable<string>? whitelist = null,
-            int banMinutes = IPBanningModule.DefaultBanMinutes,
-            int maxRequestPerSecond = IPBanningModule.DefaultMaxRequestsPerSecond,
-            int maxMatchCount = IPBanningModule.DefaultMaxMatchCount,
-            int secondsMatchingPeriod = IPBanningModule.DefaultSecondsMatchingPeriod)
-            where TContainer : class, IWebModuleContainer =>
-            SetModule(@this, failRegex, whitelist, banMinutes, maxRequestPerSecond, maxMatchCount, secondsMatchingPeriod);
-
-        private static TContainer SetModule<TContainer>(this TContainer @this,
-            IEnumerable<string>? failRegex,
-            IEnumerable<string>? whitelist = null,
-            int banMinutes = IPBanningModule.DefaultBanMinutes,
-            int maxRequestPerSecond = IPBanningModule.DefaultMaxRequestsPerSecond,
-            int maxMatchCount = IPBanningModule.DefaultMaxMatchCount,
-            int secondsMatchingPeriod = IPBanningModule.DefaultSecondsMatchingPeriod,
-            Action<IPBanningModule>? configure = null)
-            where TContainer : class, IWebModuleContainer =>
-            WithModule(@this, new IPBanningModule("/", failRegex, whitelist, banMinutes,maxRequestPerSecond, maxMatchCount, secondsMatchingPeriod), configure);
+            WithModule(@this, new IPBanningModule("/", whiteList, banMinutes), configure);
     }
 }
