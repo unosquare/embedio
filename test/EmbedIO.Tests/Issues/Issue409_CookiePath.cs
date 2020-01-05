cc5a2ee637484e8ebae58d46bd10d1de7acbf259
Geo Perez <1775792+geoperez@users.noreply.github.com>
2020-01-05 15:42:13 -0600

    [ADD] Cookie Path unit test failing

---
+++ test/EmbedIO.Tests/Issues/Issue409_CookiePath.cs
@@ -0,0 +1,51 @@
+﻿using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EmbedIO.Tests.Issues
+{
+    public class Issue409_CookiePath
+    {
+        [Test]
+        public async Task SessionModule_Handle_SameCookieBetweenModules()
+        {
+            const string DefaultUrl = "http://localhost:1234/";
+
+            using var server = new WebServer(HttpListenerMode.EmbedIO, DefaultUrl);
+            server
+                .WithLocalSessionManager()
+                .WithWebApi("/v1", o => o.WithController<CookieController>())
+                .WithWebApi("/v2", o => o.WithController<CookieController>());
+
+            _ = server.RunAsync();
+
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = new CookieContainer(),
+            };
+
+            using var client = new HttpClient(handler);
+
+            using var responseOne = await client.GetAsync($"{DefaultUrl}v1/test").ConfigureAwait(false);
+            var responseOneString = await responseOne.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            using var responseTwo = await client.GetAsync($"{DefaultUrl}v2/test").ConfigureAwait(false);
+            var responseTwoString = await responseTwo.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            Assert.AreEqual(responseOneString, responseTwoString);
+        }
+
+        private class CookieController : WebApiController
+        {
+            [Route(HttpVerbs.Get, "/test")]
+            public async Task Get()
+            {
+                Session["key"] = 1;
+                await HttpContext.SendDataAsync(Session.Id);
+            }
+        }
+    }
+}
