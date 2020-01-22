b076d52afc9c685cda15a45b3acc58be8f5156c6
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-22 18:02:10 +0100

    [FIX] Non-nullability of IHttpContext.Session.

--- src/EmbedIO/Net/Internal/HttpListenerContext.cs
+++ src/EmbedIO/Net/Internal/HttpListenerContext.cs
@@ -36,6 +36,7 @@ namespace EmbedIO.Net.Internal
             LocalEndPoint = Request.LocalEndPoint;
             RemoteEndPoint = Request.RemoteEndPoint;
             Route = RouteMatch.None;
+            Session = SessionProxy.None;
         }
 
         public string Id { get; }
---
+++ src/EmbedIO/Sessions/Internal/DummySessionProxy.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmbedIO.Sessions.Internal
+{
+    internal sealed class DummySessionProxy : ISessionProxy
+    {
+        private DummySessionProxy()
+        {
+        }
+
+        public static ISessionProxy Instance { get; } = new DummySessionProxy();
+
+        public bool Exists => false;
+
+        /// <inheritdoc/>
+        public string Id => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public TimeSpan Duration => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public DateTime LastActivity => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public int Count => 0;
+
+        /// <inheritdoc/>
+        public bool IsEmpty => true;
+
+        /// <inheritdoc/>
+        public object this[string key]
+        {
+            get => throw NoSessionManager();
+            set => throw NoSessionManager();
+        }
+
+        /// <inheritdoc/>
+        public void Delete()
+        {
+        }
+
+        /// <inheritdoc/>
+        public void Regenerate() => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public void Clear()
+        {
+        }
+
+        /// <inheritdoc/>
+        public bool ContainsKey(string key) => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public bool TryGetValue(string key, out object value) => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public bool TryRemove(string key, out object value) => throw NoSessionManager();
+
+        /// <inheritdoc/>
+        public IReadOnlyList<KeyValuePair<string, object>> TakeSnapshot() => throw NoSessionManager();
+
+        private InvalidOperationException NoSessionManager() => new InvalidOperationException("No session manager registered in the web server.");
+    }
+}
