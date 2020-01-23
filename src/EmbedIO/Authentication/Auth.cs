0a53cf57e1b123cc5d7912cd064d0d2f83fce258
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-23 15:45:08 +0100

    [ADD] Auth class and Auth.NoUser "constant".

---
+++ src/EmbedIO/Authentication/Auth.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Principal;
+
+namespace EmbedIO.Authentication
+{
+    /// <summary>
+    /// Provides useful authentication-related constants.
+    /// </summary>
+    public static class Auth
+    {
+        /// <summary>
+        /// Gets an <see cref="IPrincipal"/> interface representing
+        /// no user. To be used instead of <see langword="null"/>
+        /// to initialize or set properties of type <see cref="IPrincipal"/>.
+        /// </summary>
+        public static IPrincipal NoUser { get; } = new GenericPrincipal(
+            new GenericIdentity(string.Empty, string.Empty),
+            null);
+    }
+}
