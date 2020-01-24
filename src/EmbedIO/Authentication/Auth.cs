978c1c08b248d64391bc55714c8508e060c5918f
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-24 01:23:10 +0100

    [ADD] Method Auth.CreateUnauthenticatedPrincipal.

--- src/EmbedIO/Authentication/Auth.cs
+++ src/EmbedIO/Authentication/Auth.cs
@@ -15,5 +15,17 @@ namespace EmbedIO.Authentication
         public static IPrincipal NoUser { get; } = new GenericPrincipal(
             new GenericIdentity(string.Empty, string.Empty),
             null);
+
+        /// <summary>
+        /// Creates and returns an <see cref="IPrincipal"/> interface
+        /// representing an unauthenticated user, with the given
+        /// authentication type.
+        /// </summary>
+        /// <param name="authenticationType">The type of authentication used to identify the user.</param>
+        /// <returns>An <see cref="IPrincipal"/> interface.</returns>
+        public static IPrincipal CreateUnauthenticatedPrincipal(string authenticationType)
+            => new GenericPrincipal(
+                new GenericIdentity(string.Empty, authenticationType),
+                null);
     }
 }
