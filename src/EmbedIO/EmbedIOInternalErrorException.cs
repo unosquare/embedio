60242a6f43e2916a9a2d7f2b95c3add62811b9b5
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-24 01:55:19 +0100

    [FIX] Nullability of inner exception.

--- src/EmbedIO/EmbedIOInternalErrorException.cs
+++ src/EmbedIO/EmbedIOInternalErrorException.cs
@@ -43,8 +43,8 @@ namespace EmbedIO
         /// </summary>
         /// <param name="message">The error message that explains the reason for the exception.</param>
         /// <param name="innerException">The exception that is the cause of the current exception,
-        /// or <see langword="null"/>  if no inner exception is specified.</param>
-        public EmbedIOInternalErrorException(string message, Exception innerException)
+        /// or <see langword="null"/> if no inner exception is specified.</param>
+        public EmbedIOInternalErrorException(string message, Exception? innerException)
             : base(message, innerException)
         {
         }
