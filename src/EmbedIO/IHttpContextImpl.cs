3ecb041888f4a4c54057166d7d1274e4563a6ae4
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-24 13:35:19 +0100

    Clarify XML docs for IHttpContextImpl.

--- src/EmbedIO/IHttpContextImpl.cs
+++ src/EmbedIO/IHttpContextImpl.cs
@@ -12,7 +12,8 @@ namespace EmbedIO
 {
     /// <summary>
     /// <para>Represents a HTTP context implementation, i.e. a HTTP context as seen internally by EmbedIO.</para>
-    /// <para>This API supports the EmbedIO infrastructure and is not intended to be used directly from your code.</para>
+    /// <para>This API mainly supports the EmbedIO infrastructure; it is not intended to be used directly from your code,
+    /// unless to address specific needs in the implementation of EmbedIO plug-ins (e.g. modules).</para>
     /// </summary>
     /// <seealso cref="IHttpContext" />
     public interface IHttpContextImpl : IHttpContext
