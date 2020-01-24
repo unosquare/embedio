bb1728d4bd8d9b90a9883d7b8510feac83b7ad76
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-24 01:31:12 +0100

    [BRK][ADD] Revamp authentication as follows:
    * Authentication modules produce IPrincipal interfaces that are assigned
      to IHttpContextImpl.User, thus integrating them with the rest
      of EmbedIO.
    
    * Two or more authentication modules can be chained.
    
    * Other authentication methods beside Basic can now be easily
      integrated by deriving from AuthenticationModuleBase.
    
    * BasicAuthenticationModuleBase now supports user roles.
    
    * No source code changes for previous users of
      BasicAuthenticationModule (but beware: empty user names are no longer
      permitted.)

--- src/EmbedIO/Authentication/BasicAuthenticationModuleExtensions.cs
+++ src/EmbedIO/Authentication/BasicAuthenticationModuleExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EmbedIO.Authentication
 {
@@ -8,12 +9,14 @@ namespace EmbedIO.Authentication
     public static class BasicAuthenticationModuleExtensions
     {
         /// <summary>
-        /// Adds a username and password to the <see cref="BasicAuthenticationModule.Accounts">Accounts</see> dictionary.
+        /// Adds a user name and password to the <see cref="BasicAuthenticationModule.Accounts">Accounts</see> dictionary
+        /// of a <see cref="BasicAuthenticationModule"/>.
         /// </summary>
         /// <param name="this">The <see cref="BasicAuthenticationModule"/> on which this method is called.</param>
         /// <param name="userName">The user name.</param>
         /// <param name="password">The password.</param>
-        /// <returns><paramref name="this"/>, with the user name and password added.</returns>
+        /// <returns><paramref name="this"/>, with the user name and password added to the
+        /// <see cref="BasicAuthenticationModule.Accounts">Accounts</see> dictionary.</returns>
         /// <exception cref="NullReferenceException"><paramref name="this"/> is <see langword="null"/>.</exception>
         /// <exception cref="ArgumentNullException"><paramref name="userName"/> is <see langword="null"/>.</exception>
         /// <exception cref="OverflowException">
@@ -27,6 +30,38 @@ namespace EmbedIO.Authentication
         public static BasicAuthenticationModule WithAccount(this BasicAuthenticationModule @this, string userName, string password)
         {
             @this.Accounts.AddOrUpdate(userName, password, (_, __) => password);
+            return @this;
+        }
+
+        /// <summary>
+        /// Adds a dictionary of user names and passwords to the <see cref="BasicAuthenticationModule.Accounts">Accounts</see> dictionary
+        /// of a <see cref="BasicAuthenticationModule"/>.
+        /// </summary>
+        /// <param name="this">The <see cref="BasicAuthenticationModule"/> on which this method is called.</param>
+        /// <param name="accounts">An enumeration of key / value pairs
+        /// representing the user names and passwords to add..</param>
+        /// <returns><paramref name="this"/>, with the user names and passwords in <paramref name="accounts"/>
+        /// added to the <see cref="BasicAuthenticationModule.Accounts">Accounts</see> dictionary.</returns>
+        /// <exception cref="NullReferenceException">
+        /// <para><paramref name="this"/> is <see langword="null"/>.</para>
+        /// <para>- or -</para>
+        /// <para><paramref name="accounts"/> is <see langword="null"/>.</para>
+        /// </exception>
+        /// <exception cref="ArgumentNullException">One or more of the keys in <paramref name="accounts"/> are <see langword="null"/>.</exception>
+        /// <exception cref="OverflowException">
+        /// <para>The <see cref="BasicAuthenticationModule.Accounts">Accounts</see> dictionary already contains
+        /// the maximum number of elements (<see cref="int.MaxValue">MaxValue</see>).</para>
+        /// </exception>
+        /// <remarks>
+        /// <para>If a <paramref name="userName"/> account already exists,
+        /// its password is replaced with <paramref name="password"/>.</para>
+        /// </remarks>
+        public static BasicAuthenticationModule WithAccounts(this BasicAuthenticationModule @this, IEnumerable<KeyValuePair<string, string>> accounts)
+        {
+            foreach (var account in accounts)
+            {
+                @this.Accounts.AddOrUpdate(account.Key, account.Value, (_, __) => account.Value);
+            }
 
             return @this;
         }
