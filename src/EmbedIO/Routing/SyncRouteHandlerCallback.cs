be08384094f39447e5762c2281c03dde2435952b
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-24 01:36:43 +0100

    Fix indefinite articles in XML docs.

--- src/EmbedIO/Routing/SyncRouteHandlerCallback.cs
+++ src/EmbedIO/Routing/SyncRouteHandlerCallback.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Base class for callbacks used to handle routed requests synchronously.
     /// </summary>
-    /// <param name="context">A <see cref="IHttpContext" /> interface representing the context of the request.</param>
+    /// <param name="context">An <see cref="IHttpContext" /> interface representing the context of the request.</param>
     /// <param name="route">The matched route.</param>
     /// <seealso cref="RouteMatch"/>
     public delegate void SyncRouteHandlerCallback(IHttpContext context, RouteMatch route);
