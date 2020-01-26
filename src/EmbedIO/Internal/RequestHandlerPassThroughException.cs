a0864ad80d7bb25b32d1b88a464066f39ed0c424
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-26 06:33:25 +0100

    Disable unuseful warnings on RequestHandlerPassThroughException.

--- src/EmbedIO/Internal/RequestHandlerPassThroughException.cs
+++ src/EmbedIO/Internal/RequestHandlerPassThroughException.cs
@@ -2,7 +2,14 @@
 
 namespace EmbedIO.Internal
 {
+// This exception is only created and handled internally,
+// so it doesn't need all the standard the bells and whistles.
+#pragma warning disable CA1032 // Add standard exception constructors
+#pragma warning disable CA1064 // Exceptions should be public
+
     internal class RequestHandlerPassThroughException : Exception
     {
     }
+#pragma warning restore CA1032
+#pragma warning restore CA1064
 }
