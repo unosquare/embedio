30a4c5cefb63a059389a83b7d9b7354b864e0d84
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-31 18:17:36 +0100

    Remove excess blank line to avoid warning.

---
+++ src/EmbedIO/Internal/DummyWebModuleContainer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+using Swan;
+using Swan.Collections;
+
+namespace EmbedIO.Internal
+{
+    internal sealed class DummyWebModuleContainer : IWebModuleContainer
+    {
+        public static readonly IWebModuleContainer Instance = new DummyWebModuleContainer();
+
+        private DummyWebModuleContainer()
+        {
+        }
+
+        public IComponentCollection<IWebModule> Modules => throw UnexpectedCall();
+
+        public ConcurrentDictionary<object, object> SharedItems => throw UnexpectedCall();
+
+        private InternalErrorException UnexpectedCall([CallerMemberName] string member = "")
+            => SelfCheck.Failure($"Unexpected call to {nameof(DummyWebModuleContainer)}.{member}.");
+    }
+}
