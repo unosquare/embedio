bd4dc84cda76db1df41757e9a689fe90dcb6db1a
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-31 11:13:30 +0100

    Fix XML docs.

---
+++ src/EmbedIO/WebModuleContainer.cs
@@ -0,0 +1,18 @@
+﻿using EmbedIO.Internal;
+
+namespace EmbedIO
+{
+    /// <summary>
+    /// Provides useful constants for dealing with module containers.
+    /// </summary>
+    public static class WebModuleContainer
+    {
+        /// <summary>
+        /// <para>Gets an <see cref="IWebModuleContainer"/> interface that does not and cannot contain
+        /// any module.</para>
+        /// <para>This field is useful to initialize non-nullable fields or properties
+        /// of type <see cref="IWebModuleContainer"/>.</para>
+        /// </summary>
+        public static readonly IWebModuleContainer None = DummyWebModuleContainer.Instance;
+    }
+}
