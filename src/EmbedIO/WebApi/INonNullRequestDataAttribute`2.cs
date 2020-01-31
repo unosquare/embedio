779cebdcc2f18dbe86b904ef9ee936e1ca61e762
Riccardo De Agostini <riccardo@tenacom.it>
2020-01-31 18:16:09 +0100

    [ADD][BRK] INonNullRequestDataAttribute<,> interface.

--- src/EmbedIO/WebApi/INonNullRequestDataAttribute`2.cs
+++ src/EmbedIO/WebApi/INonNullRequestDataAttribute`2.cs
@@ -5,14 +5,14 @@ namespace EmbedIO.WebApi
     /// <summary>
     /// <para>Represents an attribute, applied to a parameter of a web API controller method,
     /// that causes the parameter to be passed deserialized data from a request.</para>
-    /// <para>The difference between this interface and <see cref="INonNullRequestDataAttribute{TController,TData}"/>
-    /// is that attributes implementing this interface can retrieve <see langword="null"/> values.</para>
+    /// <para>The difference between this interface and <see cref="IRequestDataAttribute{TController,TData}"/>
+    /// is that attributes implementing this interface never retrieve <see langword="null"/> values.</para>
     /// </summary>
     /// <typeparam name="TController">The type of the controller.</typeparam>
     /// <typeparam name="TData">The type of the data.</typeparam>
     /// <seealso cref="IRequestDataAttribute{TController}"/>
-    /// <seealso cref="INonNullRequestDataAttribute{TController,TData}"/>
-    public interface IRequestDataAttribute<in TController, TData>
+    /// <seealso cref="IRequestDataAttribute{TController,TData}"/>
+    public interface INonNullRequestDataAttribute<in TController, TData>
         where TController : WebApiController
         where TData : class
     {
@@ -23,6 +23,6 @@ namespace EmbedIO.WebApi
         /// <param name="parameterName">The name of the parameter that has to receive the data.</param>
         /// <returns>a <see cref="Task"/> whose result will be the data
         /// to pass as a parameter to a controller method.</returns>
-        Task<TData?> GetRequestDataAsync(TController controller, string parameterName);
+        Task<TData> GetRequestDataAsync(TController controller, string parameterName);
     }
 }
