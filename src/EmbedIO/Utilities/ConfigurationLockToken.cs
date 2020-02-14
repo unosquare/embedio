eecbcc5496a403ece279e366307fb5c545594320
Riccardo De Agostini <riccardo@tenacom.it>
2020-02-14 09:28:07 +0100

    [ADD] Support for token-based configuration locking.

---
+++ src/EmbedIO/Utilities/ConfigurationLockToken.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace EmbedIO.Utilities
+{
+    /// <summary>
+    /// Propagates notification that the configuration of an object has been locked.
+    /// </summary>
+    /// <remarks>
+    /// <para>A <see cref="ConfigurationLockToken"/> is usually created through the
+    /// <see cref="ConfigurationLockSource.Token">Token</see> property of an instance of
+    /// <see cref="ConfigurationLockSource"/>.</para>
+    /// <para>Once locked, a token will never transition to a non-locked state.</para>
+    /// <para>The special token named <see cref="None"/>, equal to
+    /// <c><see langword="default"/>(ConfigurationLockToken)</c>, is the only token
+    /// that can never be locked.</para>
+    /// <para>All members of this <see langword="struct"/> are thread-safe
+    /// and may be used concurrently from multiple threads.</para>
+    /// </remarks>
+    /// <seealso cref="ConfigurationLockSource"/>
+    [ComVisible(false)]
+    [DebuggerDisplay("IsLocked = {IsLocked}")]
+    public struct ConfigurationLockToken : IEquatable<ConfigurationLockToken>
+    {
+        #region Private data
+
+        readonly ConfigurationLockSource? _source;
+
+        #endregion
+
+        #region Instance management
+
+        internal ConfigurationLockToken(ConfigurationLockSource source)
+        {
+            _source = Validate.NotNull(nameof(source), source);
+        }
+
+        #endregion
+
+        #region Public API
+
+        /// <summary>
+        /// <para>Gets an empty <see cref="ConfigurationLockToken"/>,
+        /// i.e. a token that has no source and will never be locked.</para>
+        /// <para>The value of this property is equal to
+        /// <c><see langword="default"/>(ConfigurationLockToken)</c>.</para>
+        /// </summary>
+        public static ConfigurationLockToken None { get; } = default;
+
+        /// <summary>
+        /// Gets whether configuration is locked for this token.
+        /// </summary>
+        public bool IsLocked => _source?.IsLocked ?? false;
+
+        /// <summary>
+        /// Throws a <see cref="ConfigurationLockedException"/> if the configuration for this token is locked.
+        /// </summary>
+        /// <exception cref="ConfigurationLockedException">The configuration is locked.</exception>
+        public void ThrowIfLocked() => _source?.ThrowIfLocked();
+
+        #endregion
+
+        #region Overrides of Object
+
+        /// <inheritdoc />
+        public override int GetHashCode() => _source == null ? 0 : _source.GetHashCode();
+
+        /// <inheritdoc />
+        public override bool Equals(object obj) => obj is ConfigurationLockToken other && Equals(other);
+
+        #endregion
+
+        #region Implementation of IEquatable<ConfigurationLockToken>
+
+        /// <summary>
+        /// Determines whether the current <see cref="ConfigurationLockToken"/> instance
+        /// is equal to the specified token.
+        /// </summary>
+        /// <param name="other">The other <see cref="ConfigurationLockToken"/> to compare with this instance.</param>
+        /// <returns><see langword="true"/> if the instances are equal; otherwise, <see langword="false"/>.
+        /// See the Remarks section for more information.</returns>
+        /// <remarks>
+        /// <para>Two configuration lock tokens are equal if any one of the following conditions is true:</para>
+        /// <list type="bullet">
+        ///   <item><term>they are associated with the same <see cref="ConfigurationLockSource"/>;</term></item>
+        ///   <item><term>the value of both tokens is <see cref="None"/>.</term></item>
+        /// </list>
+        /// </remarks>
+        public bool Equals(ConfigurationLockToken other) => _source != null && _source == other._source;
+
+        #endregion
+
+        #region Operators
+
+        /// <summary>
+        /// Determines whether two <see cref="ConfigurationLockToken"/> instances are equal.
+        /// </summary>
+        /// <param name="a">The first instance.</param>
+        /// <param name="b">The second instance.</param>
+        /// <returns><see langword="true"/> if the instances are equal; otherwise, <see langword="false"/>.
+        /// For the definition of equality, see the <see cref="Equals(ConfigurationLockToken)">Equals</see> method.</returns>
+        /// <seealso cref="operator!="/>
+        public static bool operator ==(ConfigurationLockToken a, ConfigurationLockToken b) => a.Equals(b);
+
+        /// <summary>
+        /// Determines whether two <see cref="ConfigurationLockToken"/> instances are not equal.
+        /// </summary>
+        /// <param name="a">The first instance.</param>
+        /// <param name="b">The second instance.</param>
+        /// <returns><see langword="true"/> if the instances are not equal; otherwise, <see langword="false"/>.
+        /// For the definition of equality, see the <see cref="Equals(ConfigurationLockToken)">Equals</see> method.</returns>
+        /// <seealso cref="operator=="/>
+        public static bool operator !=(ConfigurationLockToken a, ConfigurationLockToken b) => !a.Equals(b);
+
+        #endregion
+    }
+}
---
+++ src/EmbedIO/Utilities/ConfigurationLockedException.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EmbedIO.Utilities
+{
+    /// <summary>
+    /// The exception that is thrown when trying to change the configuration of an object
+    /// after it has been locked.
+    /// </summary>
+    [Serializable]
+    public class ConfigurationLockedException : InvalidOperationException
+    {
+        #region Instance management
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        public ConfigurationLockedException()
+            : this(default(ConfigurationLockToken))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ConfigurationLockedException(string? message)
+            : this(message, default(ConfigurationLockToken))
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.
+        /// If this parameter is not <see langword="null"/>, the current exception is raised in a catch block
+        /// that handles the inner exception.</param>
+        public ConfigurationLockedException(string? message, Exception? innerException)
+            : this(message, innerException, default)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        /// <param name="token">A <see cref="ConfigurationLockToken"/> associated with the locked configuration.</param>
+        public ConfigurationLockedException(ConfigurationLockToken token)
+            : base("The configuration of an object is locked and cannot be further changed.")
+        {
+            ConfigurationLockToken = token;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="token">A <see cref="ConfigurationLockToken"/> associated with the locked configuration.</param>
+        public ConfigurationLockedException(string? message, ConfigurationLockToken token)
+            : base(message)
+        {
+            ConfigurationLockToken = token;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.
+        /// If this parameter is not <see langword="null"/>, the current exception is raised in a catch block
+        /// that handles the inner exception.</param>
+        /// <param name="token">A <see cref="ConfigurationLockToken"/> associated with the locked configuration.</param>
+        public ConfigurationLockedException(string? message, Exception? innerException, ConfigurationLockToken token)
+            : base(message, innerException)
+        {
+            ConfigurationLockToken = token;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationLockedException"/> class.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        protected ConfigurationLockedException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        #endregion
+
+        #region Public API
+
+        /// <summary>
+        /// Gets a <see cref="ConfigurationLockToken"/> associated with the locked configuration.
+        /// </summary>
+        public ConfigurationLockToken ConfigurationLockToken { get; }
+
+        #endregion
+    }
+}
