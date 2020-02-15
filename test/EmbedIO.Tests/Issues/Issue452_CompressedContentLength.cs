f4ddd49487f2a12ae4ccb31e2ad298a51c13e47f
Riccardo De Agostini <riccardo@tenacom.it>
2020-02-15 08:05:06 +0100

    Add regression test for issue #452.

---
+++ test/EmbedIO.Tests/Issues/Issue452_CompressedContentLength.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using EmbedIO.Utilities;
+using NUnit.Framework;
+
+namespace EmbedIO.Tests.Issues
+{
+    [TestFixture]
+    public class Issue452_CompressedContentLength : FileModuleTest
+    {
+        [Test]
+        public async Task GetCompressedFile_Succeeds()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, UrlPath.Root);
+
+            // Force server to use gzip compression, in order to trigger issue #452
+            request.Headers.AcceptEncoding.Clear();
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            using var response = await Client.SendAsync(request);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Status Code OK");
+        }
+    }
+}
