12fa40fc30a81321fa33b168f6df1536084decaa
Riccardo De Agostini <riccardo@tenacom.it>
2020-03-14 08:26:20 +0100

    [ADD] Parametric JSON response serializers.

--- src/packages/EmbedIO/ResponseSerializer.cs
+++ src/packages/EmbedIO/ResponseSerializer.cs
@@ -1,6 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 using EmbedIO.Utilities;
+using Swan.Formatters;
 
 namespace EmbedIO
 {
@@ -12,7 +14,7 @@ namespace EmbedIO
     {
         /// <summary>
         /// <para>The default response serializer callback used by EmbedIO.</para>
-        /// <para>Equivalent to <see cref="Json"/>.</para>
+        /// <para>Equivalent to <see cref="Json(IHttpContext,object?)">Json</see>.</para>
         /// </summary>
         public static readonly ResponseSerializerCallback Default = Json;
 
@@ -28,6 +30,41 @@ namespace EmbedIO
             Validate.NotNull(nameof(context), context).Response.ContentType = MimeType.Json;
             using var text = context.OpenResponseText(new UTF8Encoding(false));
             await text.WriteAsync(Swan.Formatters.Json.Serialize(data)).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Serializes data in JSON format with the specified <paramref name="jsonSerializerCase"/>
+        /// to a HTTP response, using the <see cref="Swan.Formatters.Json"/> utility class.
+        /// </summary>
+        /// <param name="jsonSerializerCase">The JSON serializer case.</param>
+        /// <returns>A <see cref="ResponseSerializerCallback"/> that can be used to serialize
+        /// data to a HTTP response.</returns>
+        public static ResponseSerializerCallback Json(JsonSerializerCase jsonSerializerCase)
+            => async (context, data) => {
+                Validate.NotNull(nameof(context), context).Response.ContentType = MimeType.Json;
+                using var text = context.OpenResponseText(new UTF8Encoding(false));
+                await text.WriteAsync(Swan.Formatters.Json.Serialize(data, jsonSerializerCase)).ConfigureAwait(false);
+            };
+
+        /// <summary>
+        /// Serializes data in JSON format with the specified <paramref name="serializerOptions"/>
+        /// to a HTTP response, using the <see cref="Swan.Formatters.Json"/> utility class.
+        /// </summary>
+        /// <param name="serializerOptions">The JSON serializer options.</param>
+        /// <returns>A <see cref="ResponseSerializerCallback"/> that can be used to serialize
+        /// data to a HTTP response.</returns>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="serializerOptions"/> is <see langword="null"/>.
+        /// </exception>
+        public static ResponseSerializerCallback Json(SerializerOptions serializerOptions)
+        {
+            _ = Validate.NotNull(nameof(serializerOptions), serializerOptions);
+            
+            return async (context, data) => {
+                Validate.NotNull(nameof(context), context).Response.ContentType = MimeType.Json;
+                using var text = context.OpenResponseText(new UTF8Encoding(false));
+                await text.WriteAsync(Swan.Formatters.Json.Serialize(data, serializerOptions)).ConfigureAwait(false);
+            };
         }
     }
 }
